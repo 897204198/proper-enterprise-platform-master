ed8e9b5f8f48a1e210d54cdc2fa8cdbcaf7a1cad
AlphaHinex <AlphaHinex@gmail.com>
2015-07-31 22:47:38 +0800

    Add common enums

---
+++ subprojects/pep-core/src/main/java/com/proper/enterprise/platform/core/enums/ActiveStatus.java
@@ -0,0 +1,18 @@
+package com.proper.enterprise.platform.core.enums;
+
+/**
+ * 激活状态
+ */
+public enum ActiveStatus {
+    
+    /**
+     *  激活
+     */
+    ACTIVE,
+    
+    /**
+     *  未激活
+     */
+    INACTIVE
+    
+}
---
+++ subprojects/pep-core/src/main/java/com/proper/enterprise/platform/core/enums/UseStatus.java
@@ -0,0 +1,18 @@
+package com.proper.enterprise.platform.core.enums;
+
+/**
+ * 使用状态
+ */
+public enum UseStatus {
+    
+    /**
+     *  启用
+     */
+    USE,
+    
+    /**
+     *  停用
+     */
+    STOP
+
+}
