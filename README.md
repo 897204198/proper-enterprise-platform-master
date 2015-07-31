5c40d24b7adcb275bba09ee4d2bf9934f37d0795
AlphaHinex <AlphaHinex@gmail.com>
2015-07-31 23:19:00 +0800

    Add technologies version of framework

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 proper-enterprise-platform
 ==========================
 
+Proper Enterprise Platform
+
+
+技术选型
+-------
+
+|Catalog|Version|
+|:--|:--|
+|JDK|oraclejdk7|
+|Servlet|3.0.1|
+|JSP|2.2|
+|JSTL|1.2|
+|Tomcat|7.0.63|
+|Spring|spring-framework 4.1.7.RELEASE|
+|Security|spring-security 4.0.1.RELEASE|
+|Logging|Slf4j 1.7.12，Logback 1.1.3|
+|Cache|ehcache 2.10.0|
+|Validator|Hibernate Validator 5.1.3.Final|
+|ORM|Hibernate ORM 4.3.10.Final|
+|数据库，事务，连接池||
+|任务调度|quartz 2.2.1|
+|Unit Test|junit 4.12, spock 0.7|
+|通用工具|guava 18.0|
+|编码及加密|common-codec，bounce-casle|
+|xml|dom4j|
+|JSON|fastjson 1.2.4|
+|消息队列||
+|前端框架||
+|自动构建|Gradle 2.5|
+|持续集成||
+|license||
+
 
 初始化新模块
 ----------
--- subprojects/pep-core/src/main/java/com/proper/enterprise/platform/core/entity/BaseEntity.java
+++ subprojects/pep-core/src/main/java/com/proper/enterprise/platform/core/entity/BaseEntity.java
@@ -21,16 +21,16 @@ public class BaseEntity implements Serializable {
     @GenericGenerator(name = "system-uuid", strategy = "uuid2")
     protected String id;
     
-    @Column(name="create_user_id", updatable=false)
+    @Column(name = "create_user_id", updatable = false)
     protected String createUserId;
     
-    @Column(name="create_time", updatable=false)
+    @Column(name = "create_time", updatable = false)
     protected String createTime = DateUtil.getCurrentDateString();
     
-    @Column(name="last_modify_user_id")
+    @Column(name = "last_modify_user_id")
     protected String lastModifyUserId;
     
-    @Column(name="last_modify_time")
+    @Column(name = "last_modify_time")
     protected String lastModifyTime = DateUtil.getCurrentDateString();
 
     public String getId() {
