543b8f713fb6ac427393ee796c9888184c3e931b
AlphaHinex <AlphaHinex@gmail.com>
2015-08-12 09:44:49 +0800

    Switch to IDEA

--- subprojects/pep-authc-common/src/main/java/com/proper/enterprise/platform/authc/service/impl/UserDetailsServiceImpl.java
+++ subprojects/pep-authc-common/src/main/java/com/proper/enterprise/platform/authc/service/impl/UserDetailsServiceImpl.java
@@ -1,14 +1,21 @@
 package com.proper.enterprise.platform.authc.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.proper.enterprise.platform.api.authc.repository.UserRepository;
+
+@Service("userDetailsManager")
 public class UserDetailsServiceImpl implements UserDetailsService {
+    
+    @Autowired
+    UserRepository repo;
 
     @Override
-    public UserDetails loadUserByUsername(String username)
-            throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // TODO Auto-generated method stub
         return null;
     }
