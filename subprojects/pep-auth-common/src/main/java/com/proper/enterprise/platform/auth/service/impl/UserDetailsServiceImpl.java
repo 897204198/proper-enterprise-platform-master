9f059e30b53f7411c09be7e72c5779250c0a7bf0
AlphaHinex <AlphaHinex@gmail.com>
2015-08-12 10:58:45 +0800

    Fix refactor bugs

--- subprojects/pep-auth-common/src/main/java/com/proper/enterprise/platform/auth/service/impl/UserDetailsServiceImpl.java
+++ subprojects/pep-auth-common/src/main/java/com/proper/enterprise/platform/auth/service/impl/UserDetailsServiceImpl.java
@@ -4,11 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
 import com.proper.enterprise.platform.api.auth.repository.UserRepository;
 
-@Service("userDetailsManager")
 public class UserDetailsServiceImpl implements UserDetailsService {
     
     @Autowired
