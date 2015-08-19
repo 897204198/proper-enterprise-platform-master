dced924525679d7ffe3cf1a4e16b6fd8d3c272cc
AlphaHinex <AlphaHinex@gmail.com>
2015-08-19 15:58:17 +0800

    Add a integration test

---
+++ subprojects/pep-auth-common/src/test/groovy/com/proper/enterprise/platform/integration/auth/entity/UserEntityIntegTest.groovy
@@ -0,0 +1,32 @@
+package com.proper.enterprise.platform.integration.auth.entity
+
+import com.proper.enterprise.platform.auth.entity.UserEntity
+import com.proper.enterprise.platform.auth.repository.UserRepository
+import com.proper.enterprise.platform.test.integration.AbstractIntegTest
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.core.IsEqual.equalTo
+
+class UserEntityIntegTest extends AbstractIntegTest {
+
+    @Autowired
+    UserRepository repo
+
+    @Test
+    public void checkUserExtendProperties() {
+        UserEntity entity = new UserEntity('hinex', '123456')
+        entity.putExtendProperty('a', '1')
+        entity.putExtendProperty('b', '2')
+        entity.putExtendProperty(['c': '3', 'd': '4'])
+        repo.save(entity)
+        
+        UserEntity user = repo.findByLoginName('hinex')
+        assert user.getExtendProperties().startsWith("{");
+        ['a':'1', 'b':'2', 'c':'3', 'd':'4'].entrySet().each { entry ->
+            assertThat user.getExtendProperty(entry.key), equalTo(entry.value)
+        }
+    }
+
+}
