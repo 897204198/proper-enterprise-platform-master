c3cbeae2767809d4ab701565055a119df1a1f373
AlphaHinex <AlphaHinex@gmail.com>
2015-08-28 11:12:06 +0800

    Query cache integration test

---
+++ subprojects/pep-webapp/src/test/groovy/com/proper/enterprise/platform/integration/webapp/cache/QueryCacheIntegTest.groovy
@@ -0,0 +1,31 @@
+package com.proper.enterprise.platform.integration.webapp.cache
+
+import com.proper.enterprise.platform.integration.webapp.dal.entity.TestEntity
+import com.proper.enterprise.platform.integration.webapp.dal.repository.TestRepository
+import com.proper.enterprise.platform.test.integration.AbstractIntegTest
+import org.junit.Before
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.CacheManager
+
+class QueryCacheIntegTest extends AbstractIntegTest {
+
+    @Autowired
+    CacheManager cacheManager
+
+    @Autowired
+    TestRepository repo
+
+    @Before
+    public void setUp() {
+        repo.save(new TestEntity('abc', '123'))
+    }
+
+    @Test
+    public void repeatQuery() {
+        repo.findByLoginName('abc')
+        repo.findByLoginName('abc')
+        repo.findByLoginName('abc')
+    }
+
+}
