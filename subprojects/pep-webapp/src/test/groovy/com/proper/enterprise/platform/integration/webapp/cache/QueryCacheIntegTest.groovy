e354dc03508bbe512cd0a4c8a3c49e66f4744d87
AlphaHinex <AlphaHinex@gmail.com>
2015-08-28 14:26:11 +0800

    Query cache integration test done

--- subprojects/pep-webapp/src/test/groovy/com/proper/enterprise/platform/integration/webapp/cache/QueryCacheIntegTest.groovy
+++ subprojects/pep-webapp/src/test/groovy/com/proper/enterprise/platform/integration/webapp/cache/QueryCacheIntegTest.groovy
@@ -3,6 +3,7 @@ package com.proper.enterprise.platform.integration.webapp.cache
 import com.proper.enterprise.platform.integration.webapp.dal.entity.TestEntity
 import com.proper.enterprise.platform.integration.webapp.dal.repository.TestRepository
 import com.proper.enterprise.platform.test.integration.AbstractIntegTest
+import net.sf.ehcache.Cache
 import org.junit.Before
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -23,10 +24,28 @@ class QueryCacheIntegTest extends AbstractIntegTest {
 
     @Test
     public void repeatQuery() {
-        println cacheManager.cacheNames
+        Cache sqc = cacheManager.getCache('org.hibernate.cache.internal.StandardQueryCache').nativeCache
+        Cache utc = cacheManager.getCache('org.hibernate.cache.spi.UpdateTimestampsCache').nativeCache
+        assert sqc.size == 0
+        assert utc.size == 0
+
         repo.findByLoginName('abc')
-        repo.findByLoginName('abc')
-        repo.findByLoginName('abc')
+        assert sqc.size == 1
+        assert utc.size == 1
+
+        def sqcHitcount = sqc.get(sqc.keys[0]).hitCount
+        def utcHitcount = utc.get(utc.keys[0]).hitCount
+
+        3.times {
+            repo.findByLoginName('abc')
+            def t1 = sqc.get(sqc.keys[0]).hitCount
+            def t2 = utc.get(utc.keys[0]).hitCount
+            assert t1 == sqcHitcount + 2
+            assert t2 == utcHitcount + 2
+            sqcHitcount = t1
+            utcHitcount = t2
+        }
+
     }
 
 }
