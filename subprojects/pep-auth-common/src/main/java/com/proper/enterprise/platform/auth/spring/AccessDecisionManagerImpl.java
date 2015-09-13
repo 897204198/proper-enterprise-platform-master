02e17c33c90ab1fbe190af7b1f48932bc148ea22
AlphaHinex <AlphaHinex@gmail.com>
2015-09-13 17:58:53 +0800

    Config attribute is not used in pep (maybe)

--- subprojects/pep-auth-common/src/main/java/com/proper/enterprise/platform/auth/spring/AccessDecisionManagerImpl.java
+++ subprojects/pep-auth-common/src/main/java/com/proper/enterprise/platform/auth/spring/AccessDecisionManagerImpl.java
@@ -1,6 +1,8 @@
 package com.proper.enterprise.platform.auth.spring;
 
 import com.proper.enterprise.platform.core.utils.StringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
@@ -14,6 +16,8 @@ import java.util.Collection;
 
 public class AccessDecisionManagerImpl implements AccessDecisionManager {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccessDecisionManagerImpl.class);
+
     @Override
     public void decide(Authentication authentication, Object object,
                     Collection<ConfigAttribute> configAttributes) throws AccessDeniedException,
@@ -26,6 +30,7 @@ public class AccessDecisionManagerImpl implements AccessDecisionManager {
         String url = filterInvocation.getRequestUrl();
         String method = filterInvocation.getHttpRequest().getMethod().toUpperCase();
         String auth = method + ":" + url;
+        LOGGER.info("Request is {}", auth);
 
         Object principal = authentication.getPrincipal();
         if (principal instanceof UserDetails) {
--- subprojects/pep-auth-common/src/main/java/com/proper/enterprise/platform/auth/spring/SecurityMetadataSourceImpl.java
+++ subprojects/pep-auth-common/src/main/java/com/proper/enterprise/platform/auth/spring/SecurityMetadataSourceImpl.java
@@ -1,55 +1,25 @@
 package com.proper.enterprise.platform.auth.spring;
 
-import com.proper.enterprise.platform.api.auth.Resource;
-import com.proper.enterprise.platform.api.auth.service.ResourceService;
-import com.proper.enterprise.platform.core.utils.StringUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.access.SecurityConfig;
-import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class SecurityMetadataSourceImpl implements FilterInvocationSecurityMetadataSource {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SecurityMetadataSourceImpl.class);
-
-    private static Map<String, Collection<ConfigAttribute>> resourceMap;
-
-    @Autowired
-    ResourceService resService;
-
     @Override
     public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
-        String requestUrl = ((FilterInvocation) object).getRequestUrl();
-        LOGGER.info("Request URL is {}", requestUrl);
-        if (resourceMap == null) {
-            loadResourceDefine();
-        }
-        return resourceMap.get(StringUtil.cleanUrl(requestUrl));
-    }
-
-    private void loadResourceDefine() {
-        if (resourceMap == null) {
-            resourceMap = new HashMap<>();
-        }
-
-        Set<Resource> resources = resService.getAllResources();
-        for (Resource resource : resources) {
-            Collection<ConfigAttribute> configAttributes = new ArrayList<>();
-
-            ConfigAttribute configAttribute = new SecurityConfig(resource.getId());
-            configAttributes.add(configAttribute);
-            resourceMap.put(resource.getUrl(), configAttributes);
-        }
+        return getAllConfigAttributes();
     }
 
     @Override
     public Collection<ConfigAttribute> getAllConfigAttributes() {
-        return null;
+        Collection<ConfigAttribute> configAttributes = new ArrayList<>();
+        ConfigAttribute configAttribute = new SecurityConfig("Config attribute is not used in pep");
+        configAttributes.add(configAttribute);
+        return configAttributes;
     }
 
     @Override
