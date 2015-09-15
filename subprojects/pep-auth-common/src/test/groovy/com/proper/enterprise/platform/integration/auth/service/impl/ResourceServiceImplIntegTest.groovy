0aa757e6002dc78c7b3132fc9c9da2181f1eb008
AlphaHinex <AlphaHinex@gmail.com>
2015-09-15 13:40:04 +0800

    Add an integration test

---
+++ subprojects/pep-auth-common/src/test/groovy/com/proper/enterprise/platform/integration/auth/service/impl/ResourceServiceImplIntegTest.groovy
@@ -0,0 +1,41 @@
+package com.proper.enterprise.platform.integration.auth.service.impl
+
+import com.proper.enterprise.platform.api.auth.Resource
+import com.proper.enterprise.platform.api.auth.service.ResourceService
+import com.proper.enterprise.platform.integration.auth.InsertDataWorker
+import com.proper.enterprise.platform.test.integration.AbstractIntegTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.test.context.support.WithMockUser
+
+class ResourceServiceImplIntegTest extends AbstractIntegTest {
+
+    @Autowired
+    InsertDataWorker worker
+
+    @Autowired
+    ResourceService service
+
+    @Before
+    public void setUp() {
+        push(worker.getBeforeDMLs())
+        executeSqls()
+    }
+
+    @After
+    public void tearDown() {
+        push(worker.getAfterDMLs())
+        executeSqls()
+    }
+
+    @Test
+    // mock with user1 defined in InsertDataWorker
+    @WithMockUser('hinex1')
+    public void getAllResourcesofCurrentUser() {
+        Set<Resource> resources = service.getAllResourcesOfCurrentUser()
+        assert resources.size() == 10
+    }
+
+}
