292a0e3c3dac8f72a36f96e66d46c7c35c0da00e
AlphaHinex <AlphaHinex@gmail.com>
2015-10-23 14:36:31 +0800

    Use ant pattern on authority check

---
+++ subprojects/pep-core/src/test/groovy/org/springframework/util/AntPathMatcherSpec.groovy
@@ -0,0 +1,25 @@
+package org.springframework.util
+
+import spock.lang.Specification
+
+class AntPathMatcherSpec extends Specification {
+
+    def "Match #path with #url is #result"() {
+        AntPathMatcher matcher = new AntPathMatcher()
+
+        expect:
+        matcher.match(url, path) == result
+
+        where:
+        result  | url                                   | path
+        true    | 'GET:/workflow/service/model/*/json'  | 'GET:/workflow/service/model/50/json'
+        false   | 'GET:/workflow/service/model/*/json'  | 'GET:/workflow/service/model/5/0/json'
+        true    | 'GET:/auth/resources'                 | 'GET:/auth/resources'
+        true    | 'GET:/auth/resources/*/*/xml'         | 'GET:/auth/resources/1/2/xml'
+        false   | 'GET:/auth/resources/*/*/xml'         | 'GET:/auth/resources/12/xml'
+        false   | 'GET:/auth/resources/*/*/xml'         | 'POST:/auth/resources/1/2/xml'
+        true    | 'GET:/workflow/service/model/3*/json' | 'GET:/workflow/service/model/3225/json'
+        false   | 'GET:/workflow/service/model/3*/json' | 'GET:/workflow/service/model/42/json'
+    }
+
+}
