521406dc4f55aed2e62ae3eda056981663acd2ef
AlphaHinex <AlphaHinex@gmail.com>
2015-11-02 15:47:37 +0800

    NoTx supports method

--- subprojects/pep-test/src/main/groovy/com/proper/enterprise/platform/test/integration/AbstractIntegTest.groovy
+++ subprojects/pep-test/src/main/groovy/com/proper/enterprise/platform/test/integration/AbstractIntegTest.groovy
@@ -1,11 +1,11 @@
-package com.proper.enterprise.platform.test.integration;
+package com.proper.enterprise.platform.test.integration
 
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.runner.RunWith
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.transaction.annotation.Transactional
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
--- subprojects/pep-test/src/main/groovy/com/proper/enterprise/platform/test/integration/annotation/NoTx.java
+++ subprojects/pep-test/src/main/groovy/com/proper/enterprise/platform/test/integration/annotation/NoTx.java
@@ -1,14 +1,14 @@
 package com.proper.enterprise.platform.test.integration.annotation;
 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-@Target(ElementType.TYPE)
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Transactional(propagation = Propagation.NOT_SUPPORTED)
 public @interface NoTx {
