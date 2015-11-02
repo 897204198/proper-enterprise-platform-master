a0d114db1bac54855c7cd2cfde9785e7e52403e9
AlphaHinex <AlphaHinex@gmail.com>
2015-11-02 09:58:17 +0800

    Add v3 test process

--- subprojects/pep-workflow/src/test/groovy/com/proper/enterprise/platform/integration/workflow/activiti/service/AssigneeService.groovy
+++ subprojects/pep-workflow/src/test/groovy/com/proper/enterprise/platform/integration/workflow/activiti/service/AssigneeService.groovy
@@ -19,4 +19,21 @@ class AssigneeService {
         return ['hinex1', 'hinex2', 'hinex3']
     }
 
+    public List getCountersignAssigneeList() {
+        LOGGER.info("Invoke into getCountersignAssigneeList method")
+        return (0..9).collect { idx ->
+            "cs-hinex$idx"
+        }
+    }
+
+    public void executionEnd(def execution) {
+        LOGGER.info("Invoke into executionEnd method.")
+        LOGGER.info("execution type is ${execution.class}")
+    }
+
+    public void taskEnd(def task) {
+        LOGGER.info("Invoke into taskEnd method.")
+        LOGGER.info("task type is ${task.class}")
+    }
+
 }
