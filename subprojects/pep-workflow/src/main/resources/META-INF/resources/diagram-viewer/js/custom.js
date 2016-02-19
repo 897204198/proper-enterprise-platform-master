1ce98f4999a21e877975c41207e26be4853575d6
AlphaHinex <AlphaHinex@gmail.com>
2016-02-19 17:02:44 +0800

    Change request context for diagram-viewer

--- subprojects/pep-workflow/src/main/resources/META-INF/resources/diagram-viewer/js/custom.js
+++ subprojects/pep-workflow/src/main/resources/META-INF/resources/diagram-viewer/js/custom.js
@@ -1,11 +1,3 @@
 'use strict';
 
-var PREFIX = '/workflow/service';
-
-var replacePrefix = function(str) {
-  return str.replace('/service', PREFIX);
-};
-
-ActivitiRest.options.processInstanceHighLightsUrl = replacePrefix(ActivitiRest.options.processInstanceHighLightsUrl);
-ActivitiRest.options.processDefinitionUrl = replacePrefix(ActivitiRest.options.processDefinitionUrl);
-ActivitiRest.options.processDefinitionByKeyUrl = replacePrefix(ActivitiRest.options.processDefinitionByKeyUrl);
+window.PREFIX = '/workflow/service';
