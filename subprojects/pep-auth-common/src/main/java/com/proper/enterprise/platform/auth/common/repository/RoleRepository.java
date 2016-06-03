7664ebcba090d25140c3395191cc9f26636209c4
AlphaHinex <AlphaHinex@gmail.com>
2016-06-03 10:11:48 +0800

    Clean code

--- subprojects/pep-auth-common/src/main/java/com/proper/enterprise/platform/auth/common/repository/RoleRepository.java
+++ subprojects/pep-auth-common/src/main/java/com/proper/enterprise/platform/auth/common/repository/RoleRepository.java
@@ -1,12 +1,8 @@
 package com.proper.enterprise.platform.auth.common.repository;
 
 import com.proper.enterprise.platform.auth.common.entity.RoleEntity;
-import com.proper.enterprise.platform.core.annotation.CacheQuery;
 import com.proper.enterprise.platform.core.repository.BaseRepository;
 
 public interface RoleRepository extends BaseRepository<RoleEntity, String> {
-
-    @CacheQuery
-    RoleEntity findByCode(String code);
 
 }
