9d3277d5c5815c75c93173e3a2e9ba14046b86ee
AlphaHinex <AlphaHinex@gmail.com>
2016-06-06 17:17:39 +0800

    Try to trigger teamcity

--- subprojects/pep-core/src/main/java/com/proper/enterprise/platform/core/PEPConstants.java
+++ subprojects/pep-core/src/main/java/com/proper/enterprise/platform/core/PEPConstants.java
@@ -3,11 +3,30 @@ package com.proper.enterprise.platform.core;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
-public class PEPConstants {
+/**
+ * 常量表
+ */
+public abstract class PEPConstants {
 
-    // version 0.1.0 => 0x 00 01 00
-    public static final long VERSION = 0x000100L;
-
+    /**
+     * 默认字符集
+     */
     public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+
+    /**
+     * 默认日期表示格式
+     */
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+
+    /**
+     * 默认日期时间表示格式
+     */
+    public static final String DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 平台版本号
+     * version 0.1.0 => 0x 00 01 00
+     */
+    public static final long VERSION = 0x000100L;
 
 }
--- subprojects/pep-core/src/main/java/com/proper/enterprise/platform/core/utils/DateUtil.java
+++ subprojects/pep-core/src/main/java/com/proper/enterprise/platform/core/utils/DateUtil.java
@@ -1,40 +1,42 @@
 package com.proper.enterprise.platform.core.utils;
 
+import com.proper.enterprise.platform.core.PEPConstants;
+
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * 日期工具类
+ */
 public class DateUtil {
-	
-	public static final String DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-	public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
-	
+
 	public static final int DAY = 1;
 	public static final int MONTH = 2;
 	public static final int YEAR = 3;
-	
+
 	private DateUtil() { }
-	
+
     public static int getCurrentYear() {
     	Calendar c = Calendar.getInstance();
     	return c.get(Calendar.YEAR);
     }
-	
+
 	public static Date toDate(String dateStr) {
-		return toDate(dateStr, DEFAULT_DATE_FORMAT);
+		return toDate(dateStr, PEPConstants.DEFAULT_DATE_FORMAT);
 	}
-	
+
 	public static Date toDateTime(String dateStr) {
-		return toDate(dateStr, DEFAULT_DATETIME_FORMAT);
+		return toDate(dateStr, PEPConstants.DEFAULT_DATETIME_FORMAT);
 	}
-	
+
 	public static Date toDate(String dateStr, String format) {
 		if(dateStr == null || dateStr.trim().equals("")) {
 			return null;
 		}
-		
+
         SimpleDateFormat formatter = new SimpleDateFormat(format);
         formatter.setLenient(false);
         Date realDate = null;
@@ -45,39 +47,39 @@ public class DateUtil {
         }
         return realDate;
 	}
-	
+
 	public static String toDateString(Date date) {
-		return toDateString(date, DEFAULT_DATE_FORMAT);
+		return toDateString(date, PEPConstants.DEFAULT_DATE_FORMAT);
 	}
-	
+
 	public static String toDateTimeString(Date date) {
-		return toDateString(date, DEFAULT_DATETIME_FORMAT);
+		return toDateString(date, PEPConstants.DEFAULT_DATETIME_FORMAT);
 	}
-	
+
 	public static String toDateString(Date date, String format) {
 		if(date == null) {
 			return null;
 		}
-		
+
         SimpleDateFormat formatter = new SimpleDateFormat(format);
         return formatter.format(date);
 	}
-	
+
 	public static Date getCurrentDate() {
 		Calendar cal = Calendar.getInstance();
 		return cal.getTime();
 	}
-	
+
 	public static String getCurrentDateString() {
 	    return toDateTimeString(getCurrentDate());
 	}
-	
+
 	/**
 	 * Plus or minus operations to date.
 	 * For example:
-	 * 
+	 *
 	 *     DateUtil.changeDate(DateUtil.getCurrentDate(), DateUtil.DAY, -3)
-	 * 
+	 *
 	 * @author Hinex
 	 * @date 2011-6-13 14:29:42
 	 * @param dateToChange date to change
@@ -90,25 +92,25 @@ public class DateUtil {
         cal.setTime(dateToChange);
         int field = 0;
         switch(unit) {
-        	case DAY: 
-        	    field = Calendar.DAY_OF_MONTH; 
+        	case DAY:
+        	    field = Calendar.DAY_OF_MONTH;
         	    break;
-        	case MONTH: 
-        	    field = Calendar.MONTH; 
+        	case MONTH:
+        	    field = Calendar.MONTH;
         	    break;
-        	case YEAR: 
-        	    field = Calendar.YEAR; 
+        	case YEAR:
+        	    field = Calendar.YEAR;
         	    break;
-        	default: 
+        	default:
         	    break;
         }
         cal.add(field, amount);
         return cal.getTime();
 	}
-	
+
 	/**
 	 * Use time string to update the time part of date
-	 * 
+	 *
 	 * @author Hinex
 	 * @date 2011-7-6 13:24:19
 	 * @param date    date time to update
@@ -118,7 +120,7 @@ public class DateUtil {
 	public Date updateTimePart(Date date, String timeStr) {
 		return toDateTime(toDateTimeString(date).replaceFirst("\\d+:\\d+:\\d+", timeStr));
 	}
-	
+
 	public static Date getYearMonth1stDate(String year, String month) {
 		Calendar c = Calendar.getInstance();
 		c.set(Calendar.YEAR, Integer.parseInt(year));
@@ -126,18 +128,18 @@ public class DateUtil {
 		c.set(Calendar.DAY_OF_MONTH, 1);
 		return c.getTime();
 	}
-	
+
 	public static Date getYearMonthLastDate(String year, String month) {
 		Date firstDate = getYearMonth1stDate(year, month);
 		return changeDate(changeDate(firstDate, MONTH, 1), DAY, -1);
 	}
-	
+
 	public static BigDecimal getYearMargin(Date start, Date end) {
 		Calendar endCal = Calendar.getInstance();
 		endCal.setTime(end);
 		Calendar startCal = Calendar.getInstance();
 		startCal.setTime(start);
-		
+
 		// compute years in this unit
 		int endYear = endCal.get(Calendar.YEAR);
 		int startYear = startCal.get(Calendar.YEAR);
@@ -150,5 +152,5 @@ public class DateUtil {
 		months += endDay>=startDay ? 0 : -1;
 		return new BigDecimal(months).divide(new BigDecimal(12), 2, BigDecimal.ROUND_HALF_UP);
 	}
-	
+
 }
