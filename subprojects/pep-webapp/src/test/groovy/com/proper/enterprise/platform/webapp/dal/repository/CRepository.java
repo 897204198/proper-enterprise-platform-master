991906eda38471b7a5753ac46d178258aeea303b
AlphaHinex <AlphaHinex@gmail.com>
2016-07-11 10:58:32 +0800

    Clean resources in pep-webapp

--- subprojects/pep-webapp/src/test/groovy/com/proper/enterprise/platform/webapp/dal/repository/CRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proper.enterprise.platform.webapp.dal.repository;
-
-import com.proper.enterprise.platform.core.repository.BaseRepository;
-import com.proper.enterprise.platform.webapp.dal.entity.CEntity;
-
-public interface CRepository extends BaseRepository<CEntity, String> {
-}
