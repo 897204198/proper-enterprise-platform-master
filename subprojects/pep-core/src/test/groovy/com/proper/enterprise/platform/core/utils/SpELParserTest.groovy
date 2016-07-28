6d05409b953d386520f719ca765b32d766ab419b
AlphaHinex <AlphaHinex@gmail.com>
2016-07-28 10:40:01 +0800

    Add SpELParser test

---
+++ subprojects/pep-core/src/test/groovy/com/proper/enterprise/platform/core/utils/SpELParserTest.groovy
@@ -0,0 +1,21 @@
+package com.proper.enterprise.platform.core.utils
+import com.proper.enterprise.platform.test.AbstractTest
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class SpELParserTest extends AbstractTest {
+
+    @Autowired
+    SpELParser parser
+
+    @Test
+    public void getBeanInSpEL() {
+        mockUser('id', 'name', 'pwdInSpEl')
+
+        String tpl = '{usergroup: {$in: [#{@hikariConfig.autoCommit}]}}'
+        def val = parser.parse(tpl)
+
+        assert val == "{usergroup: {\$in: [${ConfCenter.get('database.autoCommit')}]}}"
+    }
+
+}
