c4f28c77ead85e4afa967f125676273f8c0dab3a
AlphaHinex <AlphaHinex@gmail.com>
2016-08-06 20:51:53 +0800

    Login needs produce type

--- subprojects/pep-auth-jwt/src/test/groovy/com/proper/enterprise/platform/auth/jwt/controller/LoginControllerTest.groovy
+++ subprojects/pep-auth-jwt/src/test/groovy/com/proper/enterprise/platform/auth/jwt/controller/LoginControllerTest.groovy
@@ -5,6 +5,7 @@ import com.proper.enterprise.platform.test.AbstractTest
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.test.context.jdbc.Sql
 
 class LoginControllerTest extends AbstractTest {
@@ -15,13 +16,18 @@ class LoginControllerTest extends AbstractTest {
     @Test
     @Sql
     public void login() {
-        mockLogin('admin', '123456', HttpStatus.OK)
-        mockLogin('admin', '1234567', HttpStatus.UNAUTHORIZED)
-        mockLogin('test', '1234567', HttpStatus.UNAUTHORIZED)
+        // all right
+        mockLogin('admin', '123456', MediaType.TEXT_PLAIN, HttpStatus.OK)
+        // wrong produce type
+        mockLogin('admin', '123456', MediaType.APPLICATION_JSON, HttpStatus.NOT_ACCEPTABLE)
+        // wrong password
+        mockLogin('admin', '1234567', MediaType.TEXT_PLAIN, HttpStatus.UNAUTHORIZED)
+        // wrong account
+        mockLogin('test', '1234567', MediaType.TEXT_PLAIN, HttpStatus.UNAUTHORIZED)
     }
 
-    private void mockLogin(String user, String pwd, HttpStatus statusCode) {
-        post('/auth/login', """{"username":"$user","pwd":"$pwd"}""", statusCode)
+    private void mockLogin(String user, String pwd, MediaType produce, HttpStatus statusCode) {
+        post('/auth/login', produce, """{"username":"$user","pwd":"$pwd"}""", statusCode)
     }
 
 }
