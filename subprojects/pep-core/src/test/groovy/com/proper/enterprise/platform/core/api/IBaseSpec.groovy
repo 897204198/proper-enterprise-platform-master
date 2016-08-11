1368a099401948fbb24d39c55986a47363f458c2
AlphaHinex <AlphaHinex@gmail.com>
2016-08-11 22:52:44 +0800

    Full coverage

---
+++ subprojects/pep-core/src/test/groovy/com/proper/enterprise/platform/core/api/IBaseSpec.groovy
@@ -0,0 +1,32 @@
+package com.proper.enterprise.platform.core.api
+
+import com.proper.enterprise.platform.core.document.BaseDocument
+import com.proper.enterprise.platform.core.entity.BaseEntity
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class IBaseSpec extends Specification {
+
+    @Unroll
+    def "Check setter of #clz works fine"() {
+        def obj = clz.newInstance()
+        obj.setId(id)
+        obj.setCreateTime(time)
+        obj.setLastModifyTime(time)
+        obj.setCreateUserId(user)
+        obj.setLastModifyUserId(user)
+
+        expect:
+        obj.getId() == id
+        obj.getCreateTime() == time
+        obj.getLastModifyTime() == time
+        obj.getCreateUserId() == user
+        obj.getLastModifyUserId() == user
+
+        where:
+        clz                 | time                  | user  | id
+        BaseEntity.class    | '2016-06-08 10:42:00' | 'a'   | '1'
+        BaseDocument.class  | '2016-06-08 10:42:00' | 'b'   | '2'
+    }
+
+}
