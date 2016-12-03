dd4e8330bfa9aea714e8ed140a5c301c2ec8bdcc
AlphaHinex <AlphaHinex@gmail.com>
2016-12-03 15:32:02 +0800

    Update log info

--- subprojects/pep-auth-jwt/src/main/java/com/proper/enterprise/platform/auth/jwt/filter/JWTVerifyFilter.java
+++ subprojects/pep-auth-jwt/src/main/java/com/proper/enterprise/platform/auth/jwt/filter/JWTVerifyFilter.java
@@ -88,7 +88,7 @@ public class JWTVerifyFilter implements Filter {
             HandlerMethod handler = handlerHolder.getHandler(req);
             ignore = hasIgnoreOnMethod(handler) || hasIgnoreOnType(handler) || inIgnorePatterns(req);
         } catch (Exception e) {
-            LOGGER.error("Find controller error!", e.getCause());
+            LOGGER.error("Could NOT find controller for {}! {}", req.getRequestURI(), e);
         }
         return ignore;
     }
