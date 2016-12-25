a431c6e4d3e2fc2e2d09958317c395990740e531
AlphaHinex <AlphaHinex@gmail.com>
2016-12-25 15:01:10 +0800

    Could use annotation to execute scheduled tasks

--- subprojects/pep-configs/src/test/groovy/com/proper/enterprise/platform/configs/scheduler/SchedulerTest.groovy
+++ subprojects/pep-configs/src/test/groovy/com/proper/enterprise/platform/configs/scheduler/SchedulerTest.groovy
@@ -1,31 +1,44 @@
 package com.proper.enterprise.platform.configs.scheduler
+
 import com.proper.enterprise.platform.test.AbstractTest
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.TaskScheduler
-import org.springframework.scheduling.support.CronTrigger
+import org.springframework.scheduling.annotation.Scheduled
 
 class SchedulerTest extends AbstractTest {
 
-    def count = 0
+    def taskCount = 0, annoCount = 0
 
     @Autowired
     TaskScheduler scheduler
 
     @Test
-    public void scheduleTest() {
+    public void scheduleWithTask() {
         def task = new Task()
-        scheduler.schedule(task, new CronTrigger("* * * * * *"))
-        sleep(2000)
-        assert count == 2
+        def sf = scheduler.scheduleAtFixedRate(task, 50)
+        sleep(90)
+        sf.cancel(true)
+        assert taskCount == 2
     }
 
     class Task implements Runnable {
 
         @Override
         void run() {
-            count ++
+            taskCount ++
         }
+    }
+
+    @Test
+    public void scheduleWithAnnotation() {
+        // method with @Scheduled will auto run
+        assert annoCount > 0
+    }
+
+    @Scheduled(fixedDelay = 50L)
+    public void addOne() {
+        annoCount ++
     }
 
 }
