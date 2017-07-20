ddca4f62356dfe2b6cad9342e10ed7ea948d2337
AlphaHinex <AlphaHinex@gmail.com>
2017-07-20 15:22:54 +0800

    Could get by catalog

--- subprojects/pep-sys/src/main/java/com/proper/enterprise/platform/sys/datadic/AbstractConcreteDataDic.java
+++ subprojects/pep-sys/src/main/java/com/proper/enterprise/platform/sys/datadic/AbstractConcreteDataDic.java
@@ -3,15 +3,21 @@ package com.proper.enterprise.platform.sys.datadic;
 import com.proper.enterprise.platform.sys.datadic.service.DataDicService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Collection;
+
 public abstract class AbstractConcreteDataDic {
 
     @Autowired
-    private DataDicService dataDicService;
+    protected DataDicService dataDicService;
 
     public DataDic get(String code) {
         return dataDicService.get(getCatalog(), code);
     }
 
     protected abstract String getCatalog();
+
+    public Collection<? extends DataDic> getAll() {
+        return dataDicService.findByCatalog(getCatalog());
+    }
 
 }
