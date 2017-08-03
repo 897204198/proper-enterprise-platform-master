0893220eb95c48e382602be18da3d01c0a0fe06f
AlphaHinex <AlphaHinex@gmail.com>
2017-08-03 16:14:52 +0800

    Clean code

--- subprojects/pep-core/pep-core/src/test/groovy/com/proper/enterprise/platform/core/controller/mock/TestController.groovy
+++ subprojects/pep-core/pep-core/src/test/groovy/com/proper/enterprise/platform/core/controller/mock/TestController.groovy
@@ -4,13 +4,11 @@ import com.proper.enterprise.platform.core.controller.BaseController
 import com.proper.enterprise.platform.core.entity.DataTrunk
 import com.proper.enterprise.platform.core.exception.ErrMsgException
 import com.proper.enterprise.platform.core.utils.DateUtil
-import com.proper.enterprise.platform.core.utils.StringUtil
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.NoHandlerFoundException
