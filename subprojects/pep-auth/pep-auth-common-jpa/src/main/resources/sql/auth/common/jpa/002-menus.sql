4751af9eab24790373d4401e1fd4eb734045797c
wyiyi <1084896032@qq.com>
2018-03-14 00:53:08 -0500

    [修改][master][pep-288] 根据前台要求修改 (#233)
    * 根据前台要求修改
    
    * add cql

--- subprojects/pep-auth/pep-auth-common-jpa/src/main/resources/sql/auth/common/jpa/002-menus.sql
+++ subprojects/pep-auth/pep-auth-common-jpa/src/main/resources/sql/auth/common/jpa/002-menus.sql
@@ -1,7 +1,7 @@
-INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth', '权限管理', 'security', 'settings_applications_black', null, '1', 'MENU_TYPE;0');
-INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-users', '用户管理', 'users', 'person_black', 'pep-auth', '0', 'MENU_TYPE;1');
-INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-functions', '功能管理', 'functions', 'functions_black', 'pep-auth', '1', 'MENU_TYPE;1');
-INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-roles', '角色管理', 'roles', 'business_black', 'pep-auth', '2', 'MENU_TYPE;1');
-INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-user-groups', '用户组管理', 'user-groups', 'supervisor_account_black', 'pep-auth', '3', 'MENU_TYPE;1');
+INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth', '权限管理', 'auth', 'settings_applications_black', null, '1', 'MENU_TYPE;0');
+INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-users', '用户管理', 'user', 'person_black', 'pep-auth', '0', 'MENU_TYPE;1');
+INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-functions', '功能管理', 'func', 'functions_black', 'pep-auth', '1', 'MENU_TYPE;1');
+INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-roles', '角色管理', 'role', 'business_black', 'pep-auth', '2', 'MENU_TYPE;1');
+INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-user-groups', '用户组管理', 'group', 'supervisor_account_black', 'pep-auth', '3', 'MENU_TYPE;1');
 
 COMMIT;
