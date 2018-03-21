19fafee99917e80fd6dfc66bdbc8080a626cc742
Alpha Hinex <AlphaHinex@gmail.com>
2018-03-21 11:37:32 +0800

    Wf check (#255)
    * Clean EditorSource
    
    * Validate assign to initiator
    
    * Validate assign to user
    
    * Validate assign to group
    
    * Validate done
    
    * For checkstyle

--- subprojects/pep-workflow/src/main/java/com/proper/enterprise/platform/workflow/controller/ModelsController.java
+++ subprojects/pep-workflow/src/main/java/com/proper/enterprise/platform/workflow/controller/ModelsController.java
@@ -1,11 +1,10 @@
 package com.proper.enterprise.platform.workflow.controller;
 
-import com.proper.enterprise.platform.core.PEPConstants;
 import com.proper.enterprise.platform.core.controller.BaseController;
-import com.proper.enterprise.platform.workflow.EditorSource;
 import com.proper.enterprise.platform.workflow.service.DeployService;
 import com.proper.enterprise.platform.workflow.service.PEPModelService;
 import com.proper.enterprise.platform.workflow.vo.PEPModelVO;
+
 import org.flowable.app.domain.editor.Model;
 import org.flowable.app.model.common.ResultListDataRepresentation;
 import org.flowable.app.service.api.ModelService;
@@ -17,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("/repository/models")
@@ -32,20 +30,6 @@ public class ModelsController extends BaseController {
     @Autowired
     private ModelService modelService;
 
-    /**
-     * According to activiti-webapp-explorer2, an initial editor source is needed when
-     * creating a new model. The RESTFul API in activiti-rest module only support a PUT
-     * method with multipart/form-data content type request to set editor source for a
-     * model, and that API is not easy to call in front-end. This class supply a POST
-     * API with nothing else to initial the editor source for an existing model.
-     */
-    @RequestMapping(value = "/{modelId}/source", method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.CREATED)
-    public void postInitEditorSource(@PathVariable String modelId, @RequestParam String name, @RequestParam(required = false) String description) {
-        repositoryService.addModelEditorSource(modelId,
-            EditorSource.initialSource(modelId, name, description).getBytes(PEPConstants.DEFAULT_CHARSET));
-    }
-
     @RequestMapping(value = "/{modelId}/deployment", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<PEPModelVO> deployModel(@PathVariable String modelId) {
@@ -58,10 +42,8 @@ public class ModelsController extends BaseController {
         return responseOfPost(new PEPModelVO(model.getId(), model.getName(), deployment.getDeploymentTime(), processDefinition.getVersion()));
     }
 
-
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public ResultListDataRepresentation getModels(@RequestParam(required = false) String filter,
-                                                  @RequestParam(required = false) String sort, @RequestParam(required = false) Integer modelType) {
+    public ResultListDataRepresentation getModels(String filter, String sort, Integer modelType) {
         return pepModelService.getModels(filter, sort, modelType);
     }
 
--- subprojects/pep-workflow/src/test/groovy/com/proper/enterprise/platform/workflow/flowable/bpmn/usertask/UserTaskTest.groovy
+++ subprojects/pep-workflow/src/test/groovy/com/proper/enterprise/platform/workflow/flowable/bpmn/usertask/UserTaskTest.groovy
@@ -8,7 +8,6 @@ import org.flowable.engine.IdentityService
 import org.flowable.engine.RuntimeService
 import org.flowable.engine.TaskService
 import org.flowable.identitylink.api.IdentityLinkInfo
-import org.flowable.task.service.impl.persistence.entity.TaskEntity
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.jdbc.Sql
@@ -32,17 +31,21 @@ class UserTaskTest extends AbstractTest {
     void validateInitiator() {
         identityService.setAuthenticatedUserId("zjl")
         def procInst = runtimeService.startProcessInstanceByKey("ValidateInitiator")
-        TaskEntity task = taskService.createTaskQuery().taskAssignee("zjl").singleResult()
-        assert null != task
+        def task = taskService.createTaskQuery().taskAssignee("zjl").singleResult()
+        assert task.name == 'assign'
         taskService.complete(task.getId())
+        task = taskService.createTaskQuery().taskAssignee("zjl").singleResult()
+        assert task.name == 'exp'
+        assert taskService.createTaskQuery().taskAssignee("test").singleResult() == null
+        taskService.complete(task.id)
         assert isProcInstEnd(procInst.getId())
     }
 
     @Test
     void validateNamedUser() {
         def procInst = runtimeService.startProcessInstanceByKey("validateNamedUser")
-        TaskEntity task = taskService.createTaskQuery().taskAssignee("677036db-6ca6-47cf-915a-563ff8d58da4").singleResult()
-        assert null != task
+        def task = taskService.createTaskQuery().taskAssignee("677036db-6ca6-47cf-915a-563ff8d58da4").singleResult()
+        assert task.name == 'validateCommonUser'
         taskService.complete(task.getId())
         assert isProcInstEnd(procInst.getId())
     }
@@ -51,9 +54,11 @@ class UserTaskTest extends AbstractTest {
     @Sql("/com/proper/enterprise/platform/workflow/identity.sql")
     void validateAssignGroup() {
         def procInst = runtimeService.startProcessInstanceByKey("validateAssignGroup")
-        def task1 = taskService.createTaskQuery().taskCandidateGroup("group1").singleResult()
-        assert null != task1
-        def task2 = taskService.createTaskQuery().taskCandidateOrAssigned("user1").singleResult()
+        def task1 = taskService.createTaskQuery().taskCandidateGroup("group2").singleResult()
+        def task2 = taskService.createTaskQuery().taskCandidateOrAssigned("user2").singleResult()
+        def task3 = taskService.createTaskQuery().taskCandidateOrAssigned("user3").singleResult()
+        assert task1.id == task2.id
+        assert task2.id == task3.id
         taskService.complete(task2.getId())
         assert isProcInstEnd(procInst.getId())
     }
@@ -62,10 +67,9 @@ class UserTaskTest extends AbstractTest {
     void validateCandidatesUser() {
         def procInst = runtimeService.startProcessInstanceByKey("validateCandidatesUser")
         def task = taskService.createTaskQuery().taskCandidateOrAssigned("5d17010b-caf0-4613-bbb6-80b93380a7ca").singleResult()
-        assert null != task
-        taskService.complete(task.getId())
         def task2 = taskService.createTaskQuery().taskCandidateOrAssigned("677036db-6ca6-47cf-915a-563ff8d58da4").singleResult()
-        assert null == task2
+        assert task.id == task2.id
+        taskService.complete(task2.getId())
         assert isProcInstEnd(procInst.getId())
     }
 
@@ -73,80 +77,92 @@ class UserTaskTest extends AbstractTest {
     @Sql("/com/proper/enterprise/platform/workflow/identity.sql")
     void testUserTaskRole() {
         def processInstance = runtimeService.startProcessInstanceByKey("testUsertaskRole")
+
         //获取当前代理人  应为空  只有角色候选人没有代理人
         def taskAssignee1 = taskService.createTaskQuery().taskAssignee("user1").singleResult()
         assert null == taskAssignee1
         //获取当前候选人user1  应无用户组任务
         def taskGroup1 = taskService.createTaskQuery().taskCandidateGroup("group1").singleResult()
         assert null == taskGroup1
-        //获取当前候选人user1有role1角色应有任务
-        def taskUser1 = taskService.createTaskQuery().taskCandidateUser("user1").singleResult()
-        assert null != taskUser1
         //获取当前候选人user2无role1角色 应查到task实例
         def taskUser2 = taskService.createTaskQuery().taskCandidateUser("user2").singleResult()
         assert null == taskUser2
+
+        def task1Name = 'role1'
+
+        //获取当前候选人user1有role1角色应有任务
+        def taskUser1 = taskService.createTaskQuery().taskCandidateUser("user1").singleResult()
+        assert taskUser1.name == task1Name
         //获取当前角色候选 应查到task实例
         def taskRole1 = taskService.createTaskQuery().taskCandidateRole("role1").singleResult()
-        assert null != taskRole1
+        assert taskRole1.name == task1Name
         //获取当前角色候选 应查到task实例
         def taskRole1In = taskService.createTaskQuery().taskCandidateRoleIn(["role1"]).singleResult()
-        assert null != taskRole1In
+        assert taskRole1In.name == task1Name
         //获取当前角色候选 应查到task实例
         def taskCanOrAss1 = taskService.createTaskQuery().ignoreAssigneeValue()
             .taskCandidateOrAssigned("user1").singleResult()
-        assert null != taskCanOrAss1
+        assert taskCanOrAss1.name == task1Name
+
         taskService.addCandidateRole(taskCanOrAss1.getId(), 'role2')
         taskService.deleteCandidateRole(taskCanOrAss1.getId(), 'role1')
+
         //动态删除候选角色 并验证role1候选任务为空
         def delTaskRole1 = taskService.createTaskQuery().taskCandidateRole("role1").singleResult()
         assert null == delTaskRole1
         //动态添加候选角色 并验证role2候选任务存在
         def addTaskRole2 = taskService.createTaskQuery().taskCandidateRole("role2").singleResult()
-        assert null != addTaskRole2
+        assert addTaskRole2.name == task1Name
         //动态添加候选角色 并验证user2,user3属于role2候选任务存在
         def addTaskUser2 = taskService.createTaskQuery().taskCandidateUser("user2").singleResult()
-        assert null != addTaskUser2
+        assert addTaskUser2.name == task1Name
         def addTaskUser3 = taskService.createTaskQuery().taskCandidateUser("user3").singleResult()
-        assert null != addTaskUser3
+        assert addTaskUser3.name == task1Name
         //user2签收任务  user3中任务为空
         taskService.claim(addTaskUser2.getId(), "user2")
+
         def claimTaskUser2 = taskService.createTaskQuery()
             .taskCandidateOrAssigned("user2").singleResult()
-        assert null != claimTaskUser2
+        assert claimTaskUser2.name == task1Name
         def claimTaskUser3 = taskService.createTaskQuery().taskCandidateUser("user3").singleResult()
         assert null == claimTaskUser3
         taskService.complete(claimTaskUser2.getId())
+
         //验证历史
         def historicIdentityLinks = historyService.getHistoricIdentityLinksForTask(claimTaskUser2.getId())
         assert null != historicIdentityLinks && historicIdentityLinks.size() > 0
-        assert haveRoleLink("role2", historicIdentityLinks)
+        assert hasRoleLink("role2", historicIdentityLinks)
+        assert !hasRoleLink('role1', historicIdentityLinks)
+
         def hisIns = historyService.createHistoricTaskInstanceQuery()
             .processInstanceId(processInstance.processInstanceId)
             .ignoreAssigneeValue()
             .includeIdentityLinks().finished().singleResult()
         assert null != hisIns
-        assert haveRoleLink('role2', hisIns.getIdentityLinks())
+        assert hasRoleLink('role2', hisIns.getIdentityLinks())
+
+        def task2Name = 'role1ORrole2'
+
         //验证双角色候选
         def taskUser1Step2 = taskService.createTaskQuery().taskCandidateUser("user1").singleResult()
-        assert null != taskUser1Step2
+        assert taskUser1Step2.name == task2Name
         def taskUser2Step2 = taskService.createTaskQuery().taskCandidateUser("user2").singleResult()
-        assert null != taskUser2Step2
+        assert taskUser2Step2.name == task2Name
         taskService.complete(taskUser1Step2.getId())
-        assert null == runtimeService.createProcessInstanceQuery()
-            .processInstanceId(processInstance.getId()).singleResult()
+        assert isProcInstEnd(processInstance.getId())
     }
 
-    private boolean haveRoleLink(String roleId, List<IdentityLinkInfo> links) {
+    private static boolean hasRoleLink(String roleId, List<IdentityLinkInfo> links) {
         for (IdentityLinkInfo link : links) {
-            if (roleId.equals(link.getRoleId())) {
+            if (roleId == link.getRoleId()) {
                 return true
             }
         }
         return false
     }
 
-
     private boolean isProcInstEnd(String procInstId) {
         runtimeService.createProcessInstanceQuery().processInstanceId(procInstId).singleResult() == null
     }
+
 }
