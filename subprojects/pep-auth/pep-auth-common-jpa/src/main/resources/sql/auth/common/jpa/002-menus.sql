3f265b9e8b4ea1d6db69c6e4b85451ae481f4070
AlphaHinex <AlphaHinex@gmail.com>
2018-03-21 14:42:56 +0800

    Adjust menus

--- subprojects/pep-auth/pep-auth-common-jpa/src/main/resources/sql/auth/common/jpa/002-menus.sql
+++ subprojects/pep-auth/pep-auth-common-jpa/src/main/resources/sql/auth/common/jpa/002-menus.sql
@@ -1,7 +1,7 @@
-INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth', '权限管理', 'auth', 'settings_applications_black', null, '1', 'MENU_TYPE;0');
-INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-users', '用户管理', 'user', 'person_black', 'pep-auth', '0', 'MENU_TYPE;1');
-INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-functions', '功能管理', 'func', 'functions_black', 'pep-auth', '1', 'MENU_TYPE;1');
-INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-roles', '角色管理', 'role', 'business_black', 'pep-auth', '2', 'MENU_TYPE;1');
-INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-user-groups', '用户组管理', 'group', 'supervisor_account_black', 'pep-auth', '3', 'MENU_TYPE;1');
+INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth', '权限管理', 'auth', 'lock', null, '1', 'MENU_TYPE;0');
+INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-users', '用户管理', 'user', 'solution', 'pep-auth', '0', 'MENU_TYPE;1');
+INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-functions', '功能管理', 'func', 'bars', 'pep-auth', '1', 'MENU_TYPE;1');
+INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-roles', '角色管理', 'role', 'skin', 'pep-auth', '2', 'MENU_TYPE;1');
+INSERT INTO pep_auth_menus (id, name, route, icon, parent_id, sequence_number, menu_type) VALUES ('pep-auth-user-groups', '用户组管理', 'group', 'team', 'pep-auth', '3', 'MENU_TYPE;1');
 
 COMMIT;
