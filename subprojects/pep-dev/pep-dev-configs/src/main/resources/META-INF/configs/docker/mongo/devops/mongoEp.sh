12b5015b8d0afee6afb5c9c8c023823848376928
Alpha Hinex <AlphaHinex@gmail.com>
2018-06-13 14:52:17 +0800

    [未完][develop][PEP-617] Mongo3.6.5 (#390)
    [完美][develop][PEP-617] Mongo3.6.5

--- subprojects/pep-dev/pep-dev-configs/src/main/resources/META-INF/configs/docker/mongo/devops/mongoEp.sh
+++ subprojects/pep-dev/pep-dev-configs/src/main/resources/META-INF/configs/docker/mongo/devops/mongoEp.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-mongod --noprealloc --smallfiles &
+mongod --noprealloc --smallfiles --replSet pep &
 PID="$!"
 
 echo "WAITING FOR INIT"
 sleep 3
 
-echo "--> init"
-mongo /code/devops/current/init.js
-echo "--> init [DONE]"
+echo "--> rs init"
+mongo /code/devops/current/init-rs.js
+echo "--> rs init [DONE]"
+
+sleep 3
+
+echo "--> user init"
+mongo /code/devops/current/init-user.js
+echo "--> user init [DONE]"
 
 kill $PID
 
 echo "WAITING FOR KILL"
 sleep 3
 
-mongod --noprealloc --smallfiles --auth
+mongod --noprealloc --smallfiles --auth --replSet pep --bind_ip_all
