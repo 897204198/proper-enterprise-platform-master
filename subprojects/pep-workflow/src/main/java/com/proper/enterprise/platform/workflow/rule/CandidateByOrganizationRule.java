7a3df3be865df57a5f15e97ef0cc4a4e093e5c13
方明 <40154851+Kirinfm@users.noreply.github.com>
2018-07-26 16:22:41 +0800

    增加 flowable 候选经办人/用户组/角色 rule (#497)

--- subprojects/pep-workflow/src/main/java/com/proper/enterprise/platform/workflow/rule/CandidateByOrganizationRule.java
+++ subprojects/pep-workflow/src/main/java/com/proper/enterprise/platform/workflow/rule/CandidateByOrganizationRule.java
@@ -2,13 +2,13 @@ package com.proper.enterprise.platform.workflow.rule;
 
 import java.util.List;
 
-public interface CandidateUserGroupByOrganizationRule {
+public interface CandidateByOrganizationRule {
 
     /**
-     * 根据组织设定候选经办用户组
+     * 根据组织设定候选经办 人/用户组/角色 集合
      *
      * @param organization 组织
-     * @return 候选经办用户组Id
+     * @return 候选经办 人/用户组/角色 集合
      */
     List<String> execute(String organization);
 }
