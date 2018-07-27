0ee337be165ad5f39eb13a9f455223cd501aa53d
SuperMan <30485055+jianlinz@users.noreply.github.com>
2018-07-27 09:17:38 +0800

    [完美][develop]待办通知支持候选人/用户组/角色 (#499)
    * 流程框架支持子流程的相关查询
    
    * 流程框架支持子流程的相关查询
    
    * 流程框架支持子流程的相关查询
    
    * 简化流程变量获取方式
    
    * 简化流程变量获取方式
    
    * 调整待办通知 支持经办人和候选人/用户组/角色
    
    * 调整待办通知 支持经办人和候选人/用户组/角色
    
    * 调整待办通知 支持经办人和候选人/用户组/角色
    
    * Merge branch 'develop' of https://github.com/propersoft-cn/proper-enterprise-platform into currUserOrgAndRole2
    
    # Conflicts:
    #	subprojects/pep-workflow/src/main/java/com/proper/enterprise/platform/workflow/service/impl/TaskAssigneeNoticeImpl.java
    
    * Merge branch 'develop' of https://github.com/propersoft-cn/proper-enterprise-platform into currUserOrgAndRole2
    
    # Conflicts:
    #	subprojects/pep-workflow/src/main/java/com/proper/enterprise/platform/workflow/service/impl/TaskAssigneeNoticeImpl.java
    
    * Merge branch 'develop' of https://github.com/propersoft-cn/proper-enterprise-platform into currUserOrgAndRole2
    
    # Conflicts:
    #	subprojects/pep-workflow/src/main/java/com/proper/enterprise/platform/workflow/service/impl/TaskAssigneeNoticeImpl.java
    
    * Merge branch 'develop' of https://github.com/propersoft-cn/proper-enterprise-platform into currUserOrgAndRole2
    
    # Conflicts:
    #	subprojects/pep-workflow/src/main/java/com/proper/enterprise/platform/workflow/service/impl/TaskAssigneeNoticeImpl.java
    
    * 完美

--- subprojects/pep-workflow/src/test/groovy/com/proper/enterprise/platform/workflow/api/notice/TaskAssigneeNoticeImpl.java
+++ subprojects/pep-workflow/src/test/groovy/com/proper/enterprise/platform/workflow/api/notice/TaskAssigneeNoticeImpl.java
@@ -1,17 +1,30 @@
 package com.proper.enterprise.platform.workflow.api.notice;
 
 import com.proper.enterprise.platform.core.security.Authentication;
-import com.proper.enterprise.platform.workflow.api.TaskAssigneeNotice;
+import com.proper.enterprise.platform.workflow.api.TaskAssigneeOrCandidateNotice;
+import org.flowable.identitylink.service.impl.persistence.entity.IdentityLinkEntity;
 import org.flowable.task.service.impl.persistence.entity.TaskEntity;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service("taskAssigneeNoticePri")
 @Primary
-public class TaskAssigneeNoticeImpl implements TaskAssigneeNotice {
+public class TaskAssigneeNoticeImpl implements TaskAssigneeOrCandidateNotice {
 
     @Override
     public void notice(TaskEntity task) {
-        Authentication.setCurrentUserId(task.getAssignee() + "1");
+        StringBuffer returnValue = new StringBuffer();
+        for (IdentityLinkEntity identityLinkEntity : task.getIdentityLinks()) {
+            if (identityLinkEntity.isUser()) {
+                returnValue.append(identityLinkEntity.getUserId());
+            }
+            if (identityLinkEntity.isGroup()) {
+                returnValue.append(identityLinkEntity.getGroupId());
+            }
+            if (identityLinkEntity.isRole()) {
+                returnValue.append(identityLinkEntity.getRoleId());
+            }
+        }
+        Authentication.setCurrentUserId(task.getAssignee() + returnValue);
     }
 }
