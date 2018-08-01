bbdde363963e5ad1eea9cd33605368700f91f53b
王浩鹏 <wanghaopeng@propersoft.cn>
2018-08-01 09:10:37 +0800

    supplement (#519)
    * supplement
    
    * supplement
    
    * supplement

--- subprojects/pep-notice/pep-notice/src/test/groovy/com/proper/enterprise/platform/notice/service/NoticeServiceImplTest.groovy
+++ subprojects/pep-notice/pep-notice/src/test/groovy/com/proper/enterprise/platform/notice/service/NoticeServiceImplTest.groovy
@@ -3,17 +3,45 @@ package com.proper.enterprise.platform.notice.service
 
 import com.proper.enterprise.platform.core.utils.JSONUtil
 import com.proper.enterprise.platform.notice.model.NoticeModel
+import com.proper.enterprise.platform.push.vo.PushChannelVO
 import com.proper.enterprise.platform.test.AbstractTest
+import org.junit.Before
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.jdbc.Sql
 
-@Sql("/com/proper/enterprise/platform/notice/sql/datadics.sql")
+@Sql("/com/proper/enterprise/platform/notice/sql/NoticeTest.sql")
 class NoticeServiceImplTest extends AbstractTest {
 
     @Autowired
     NoticeService noticeService
+
+    def url = "/push/channels"
+
+    @Before
+    void init() {
+        PushChannelVO pushChannelVo = new PushChannelVO()
+        pushChannelVo.setChannelName("test")
+        pushChannelVo.setChannelDesc("推送平台test")
+        pushChannelVo.setMsgSaveDays(3)
+        pushChannelVo.setMaxSendCount(5)
+        pushChannelVo.setSecretKey("b2024e00064bc5d8db70fdee087eae4f")
+        pushChannelVo.setAndroid(new PushChannelVO.Android(
+            new PushChannelVO.Android.HuaweiBean(
+                "10819197", "fbfe31923440e417f8fb9f4ce133e3c1",
+                "com.proper.mobile.oa.shengjing.htest"),
+            new PushChannelVO.Android.XiaomiBean("2AF1VndMLqwLF/4zOHgWNw==",
+                "com.proper.mobile.oa.shengjing.htest")))
+        pushChannelVo.setIos(new PushChannelVO.IOS(false, "h123456",
+            "com.proper.mobile.oa.shengjing.htest"))
+        this.addChannel(pushChannelVo)
+    }
+
+    PushChannelVO addChannel(PushChannelVO pushChannelVo) {
+        def resultContent = post(url, com.proper.enterprise.platform.test.utils.JSONUtil.toJSON(pushChannelVo), HttpStatus.CREATED).getResponse().getContentAsString()
+        return com.proper.enterprise.platform.test.utils.JSONUtil.parse(resultContent, PushChannelVO.class)
+    }
 
     @Test
     void sendNotice() {
@@ -21,7 +49,7 @@ class NoticeServiceImplTest extends AbstractTest {
         custom.put("pageUrl", "messages")
 
         NoticeModel noticeModel1 = new NoticeModel();
-        noticeModel1.setSystemId("ihos")
+        noticeModel1.setSystemId("test")
         noticeModel1.setBusinessId("testBpm")
         noticeModel1.setBusinessName("测试流程")
         noticeModel1.setNoticeType("BPM")
