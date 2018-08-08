60dfaa48fd0bfa1a189b39004b25be4680fd6abf
guozhimin-proper <41357385+guozhimin-proper@users.noreply.github.com>
2018-08-08 14:12:38 +0800

    [完美][develop]推送增加pushID日志：将入库和推送操作分开 (#541)
    * [推送重构本地保存]
    
    * [推送重构；本地保存]
    
    * [推送重构本地保存]
    
    * [推送重构保存]
    
    * 【推送重构本地保存】
    
    * 【推送重构本地保存】
    
    * 【推送重构本地保存】
    
    * 【增加日志信息】

--- subprojects/pep-push/pep-push/src/main/java/com/proper/enterprise/platform/push/service/PushMsgService.java
+++ subprojects/pep-push/pep-push/src/main/java/com/proper/enterprise/platform/push/service/PushMsgService.java
@@ -6,6 +6,10 @@ import com.proper.enterprise.platform.push.api.PushMsg;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Pageable;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public interface PushMsgService extends BaseJpaService<PushMsg, String> {
     /**
      * 根据查询条件获取推送消息集合
@@ -15,4 +19,38 @@ public interface PushMsgService extends BaseJpaService<PushMsg, String> {
      * @return List 推送消息集合
      */
     DataTrunk<? extends PushMsg> findByDateTypeAndAppkey(Example example, Pageable pageable);
+
+    /**
+     * 推送消息到指定用户，消息入库
+     *
+     * @param msgData 消息数据
+     * @return List 推送消息ID的集合
+     */
+    List savePushMessageToUsers(Map<String, String> msgData);
+
+    /**
+     * 推送消息到一组设备上，这一组设备需要要相同的设备类型
+     *
+     * @param msgData 消息数据
+     * @return List 推送消息ID的集合
+     */
+    List savePushMessageToDevices(Map<String, String> msgData);
+
+    /**
+     * 推送消息到全部用户
+     *
+     * @param msgData 消息数据
+     * @return List 推送消息ID的集合
+     * @throws IOException 集合转JSON异常
+     */
+    List savePushMessageToAllUsers(Map<String, String> msgData) throws IOException;
+
+    /**
+     * 推送消息到指定类型的全部设备
+     *
+     * @param msgData 消息数据
+     * @return List 推送消息ID的集合
+     * @throws IOException 集合转JSON异常
+     */
+    List savePushMessageToAllDevices(Map<String, String> msgData) throws IOException;
 }
--- subprojects/pep-push/pep-push/src/main/java/com/proper/enterprise/platform/push/service/impl/PushMsgServiceImpl.java
+++ subprojects/pep-push/pep-push/src/main/java/com/proper/enterprise/platform/push/service/impl/PushMsgServiceImpl.java
@@ -2,25 +2,43 @@ package com.proper.enterprise.platform.push.service.impl;
 
 import com.proper.enterprise.platform.core.entity.DataTrunk;
 import com.proper.enterprise.platform.core.jpa.service.impl.AbstractJpaServiceSupport;
+import com.proper.enterprise.platform.core.utils.CollectionUtil;
+import com.proper.enterprise.platform.core.utils.JSONUtil;
+import com.proper.enterprise.platform.core.utils.StringUtil;
 import com.proper.enterprise.platform.push.api.PushMsg;
+import com.proper.enterprise.platform.push.api.openapi.exceptions.PushException;
+import com.proper.enterprise.platform.push.api.openapi.model.PushMessage;
+import com.proper.enterprise.platform.push.common.model.enums.PushDeviceStatus;
+import com.proper.enterprise.platform.push.common.model.enums.PushDeviceType;
+import com.proper.enterprise.platform.push.common.model.enums.PushMode;
 import com.proper.enterprise.platform.push.config.PushGlobalInfo;
+import com.proper.enterprise.platform.push.entity.PushDeviceEntity;
 import com.proper.enterprise.platform.push.entity.PushMsgEntity;
+import com.proper.enterprise.platform.push.entity.PushUserEntity;
+import com.proper.enterprise.platform.push.repository.PushDeviceRepository;
 import com.proper.enterprise.platform.push.repository.PushMsgRepository;
+import com.proper.enterprise.platform.push.repository.PushUserRepository;
 import com.proper.enterprise.platform.push.service.PushMsgService;
+import com.proper.enterprise.platform.push.vendor.AbstractPushVendorService;
+import com.proper.enterprise.platform.push.vendor.PushVendorFactory;
 import com.proper.enterprise.platform.push.vo.PushMsgVO;
 import com.proper.enterprise.platform.sys.datadic.DataDic;
 import com.proper.enterprise.platform.sys.datadic.util.DataDicUtil;
+import org.nutz.json.Json;
 import org.nutz.mapl.Mapl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class PushMsgServiceImpl extends AbstractJpaServiceSupport<PushMsg, PushMsgRepository, String>
@@ -29,6 +47,12 @@ public class PushMsgServiceImpl extends AbstractJpaServiceSupport<PushMsg, PushM
 
     @Autowired
     PushGlobalInfo globalInfo;
+    @Autowired
+    PushDeviceRepository deviceRepo;
+    @Autowired
+    PushVendorFactory pushVendorFactory;
+    @Autowired
+    PushUserRepository pushUserRepository;
     @Autowired
     private PushMsgRepository pushMsgRepository;
 
@@ -41,6 +65,155 @@ public class PushMsgServiceImpl extends AbstractJpaServiceSupport<PushMsg, PushM
         Page<PushMsgEntity> page = pushMsgRepository.findAll(example, pageable);
         DataTrunk<? extends PushMsg> trunk = new DataTrunk<>(convertVo(page), page.getTotalElements());
         return trunk;
+    }
+
+    @Override
+    public List savePushMessageToUsers(Map<String, String> requestParams) {
+        List<String> idList = new ArrayList<>();
+        String userids = requestParams.get("userids");
+        String appkey = requestParams.get("appkey");
+        final List<String> lstUids = Json.fromJsonAsList(String.class, userids);
+        final PushMessage thePushmsg = getPushMessage(requestParams);
+
+        Map<String, Object> deviceConf = (Map<String, Object>) Mapl.cell(globalInfo.getPushConfigs(),
+            appkey + ".device");
+        if (deviceConf == null) {
+            throw new PushException("appkey: " + appkey + " is not valid!msg:" + JSONUtil.toJSONIgnoreException(thePushmsg));
+        }
+
+        for (Map.Entry<String, Object> entry : deviceConf.entrySet()) {
+            String strDeviceType = entry.getKey();
+            PushDeviceType deviceType = Enum.valueOf(PushDeviceType.class, strDeviceType.trim());
+            Map<String, Object> mapPushModes = (Map<String, Object>) entry.getValue();
+            for (String strPushMode : mapPushModes.keySet()) {
+                PushMode pushMode = Enum.valueOf(PushMode.class, strPushMode.trim());
+                AbstractPushVendorService pushService = pushVendorFactory.getPushVendorService(appkey, deviceType,
+                    pushMode);
+                List<PushDeviceEntity> lstDevices = null;
+                // 查找存在的用户id对应的有效的设备
+                if (CollectionUtil.isNotEmpty(lstUids)) {
+                    lstDevices = deviceRepo.findByAppkeyAndDevicetypeAndPushModeAndMstatusAndUseridIn(appkey,
+                        deviceType, pushMode, PushDeviceStatus.VALID, lstUids);
+                    List<PushMsgEntity> list = doSavePushMsg(appkey, thePushmsg, deviceType, pushMode, lstDevices);
+                    list.forEach(n -> idList.add(n.getId()));
+                } else {
+                    LOGGER.error("savePushMessageToUsers but the userid is empty!msg:{}", JSONUtil.toJSONIgnoreException(thePushmsg));
+                }
+
+            }
+        }
+        return idList;
+    }
+
+    @Override
+    public List savePushMessageToDevices(Map<String, String> requestParams) {
+        List<String> idList = new ArrayList<>();
+        String deviceids = requestParams.get("deviceids");
+        String strDeviceType = requestParams.get("device_type");
+        String appkey = requestParams.get("appkey");
+        final List<String> lstDeviceids = Json.fromJsonAsList(String.class, deviceids);
+        final PushMessage thePushmsg = getPushMessage(requestParams);
+        PushDeviceType deviceType = null;
+        if (StringUtil.isNotEmpty(strDeviceType)) {
+            deviceType = Enum.valueOf(PushDeviceType.class, strDeviceType.trim());
+        }
+        Map<String, Object> deviceConf = (Map<String, Object>) Mapl.cell(globalInfo.getPushConfigs(),
+            appkey + ".device");
+        if (deviceConf == null) {
+            throw new PushException("appkey: " + appkey + " is not valid!msg:" + JSONUtil.toJSONIgnoreException(thePushmsg));
+        }
+        Map<String, Object> mapPushModes = (Map<String, Object>) deviceConf.get(deviceType.toString());
+        for (String strPushMode : mapPushModes.keySet()) {
+            PushMode pushMode = Enum.valueOf(PushMode.class, strPushMode.trim());
+            AbstractPushVendorService pushService = pushVendorFactory.getPushVendorService(appkey, deviceType,
+                pushMode);
+            // 查找存在的deviceid对应的有效的设备
+            if (CollectionUtil.isNotEmpty(lstDeviceids)) {
+                List<PushDeviceEntity> lstDevices = deviceRepo.findByAppkeyAndDevicetypeAndPushModeAndMstatusAndDeviceidIn(appkey, deviceType,
+                    pushMode, PushDeviceStatus.VALID, lstDeviceids);
+                List<PushMsgEntity> list = doSavePushMsg(appkey, thePushmsg, deviceType, pushMode, lstDevices);
+                list.forEach(n -> idList.add(n.getId()));
+            } else {
+                LOGGER.error("savePushMessageToDevices but the deviceids is empty!");
+            }
+        }
+        return idList;
+    }
+
+    @Override
+    public List savePushMessageToAllUsers(Map<String, String> requestParams) throws IOException {
+        List<String> idList = new ArrayList<>();
+        String appkey = requestParams.get("appkey");
+        final int dbBatchSize = globalInfo.getDbBatchSize();
+        Pageable pageable = new PageRequest(0, dbBatchSize);
+        List<String> lstUids = new ArrayList<>(dbBatchSize);
+        boolean hasData;
+        do {
+            Page<PushUserEntity> page = pushUserRepository.findByAppkey(appkey, pageable);
+            pageable = pageable.next();
+
+            hasData = page.hasContent();
+            if (hasData) {
+                List<PushUserEntity> lstUser = page.getContent();
+                lstUids.clear();
+                lstUser.forEach(userEntity -> lstUids.add(userEntity.getUserid()));
+                requestParams.put("userids", JSONUtil.toJSON(lstUids).toString());
+                List<String> list = savePushMessageToUsers(requestParams);
+                idList.addAll(list);
+            }
+        } while (hasData);
+        return idList;
+    }
+
+    @Override
+    public List savePushMessageToAllDevices(Map<String, String> requestParams) throws IOException {
+        List<String> idList = new ArrayList<>();
+
+        String appkey = requestParams.get("appkey");
+        String strDeviceType1 = requestParams.get("device_type");
+        PushDeviceType deviceType0 = null;
+        if (StringUtil.isNotEmpty(strDeviceType1)) {
+            deviceType0 = Enum.valueOf(PushDeviceType.class, strDeviceType1.trim());
+        }
+
+        Map<String, Object> deviceConf = (Map<String, Object>) Mapl.cell(globalInfo.getPushConfigs(),
+            appkey + ".device");
+        if (deviceConf == null) {
+            throw new PushException("appkey: " + appkey + " is not valid!");
+        }
+        final int dbBatchSize = globalInfo.getDbBatchSize();
+        List<String> lstDids = new ArrayList<>(dbBatchSize);
+        boolean hasData = false;
+        for (String strDeviceType : deviceConf.keySet()) {
+
+            PushDeviceType deviceType = Enum.valueOf(PushDeviceType.class, strDeviceType.trim());
+            // 传入的deviceType不为空，则只发送指定设备类型的消息
+            if (deviceType0 != null && deviceType != deviceType0) {
+                continue;
+            }
+            Pageable pageable = new PageRequest(0, dbBatchSize);
+            do {
+                Page<PushDeviceEntity> page = deviceRepo.findByAppkeyAndDevicetypeAndMstatus(appkey, deviceType,
+                    PushDeviceStatus.VALID, pageable);
+                pageable = pageable.next();
+
+                hasData = page.hasContent();
+                if (hasData) {
+                    List<PushDeviceEntity> lstDevice = page.getContent();
+                    lstDids.clear();
+                    lstDevice.forEach(deviceEntity -> lstDids.add(deviceEntity.getDeviceid()));
+                    requestParams.put("device_type", deviceType.toString());
+                    requestParams.put("deviceids", JSONUtil.toJSON(lstDids).toString());
+                    List<String> list = savePushMessageToDevices(requestParams);
+                    idList.addAll(list);
+                }
+            } while (hasData);
+            // 传入的deviceType不为空，则只发送指定设备类型的消息
+            if (deviceType0 != null) {
+                break;
+            }
+        }
+        return idList;
     }
 
     private List<PushMsgVO> convertVo(Page<PushMsgEntity> page) {
@@ -60,5 +233,44 @@ public class PushMsgServiceImpl extends AbstractJpaServiceSupport<PushMsg, PushM
             voList.add(vo);
         }
         return voList;
+    }
+
+    private PushMessage getPushMessage(Map<String, String> requestParams) {
+        String smsg = requestParams.get("msg");
+        final PushMessage thePushmsg;
+        try {
+            thePushmsg = JSONUtil.parse(smsg, PushMessage.class);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+            throw new PushException(e.getMessage());
+        }
+        return thePushmsg;
+    }
+
+    private List<PushMsgEntity> doSavePushMsg(String appkey, PushMessage thePushmsg, PushDeviceType deviceType, PushMode pushMode,
+                                              List<PushDeviceEntity> lstDevices) {
+        List<PushMsgEntity> saveResultList = new ArrayList<>();
+        if (CollectionUtil.isNotEmpty(lstDevices)) {
+            List<PushMsgEntity> lstMsgs = new ArrayList<PushMsgEntity>();
+            for (PushDeviceEntity device : lstDevices) {
+                if (StringUtil.isNotEmpty(device.getPushToken())) {
+                    PushMsgEntity dbMsg = new PushMsgEntity(thePushmsg, appkey, device);
+                    lstMsgs.add(dbMsg);
+                    LOGGER.info("PushMsgServiceImpl doSavePushMsg pushId:{},pushEntity:{}",
+                        dbMsg.getId(), JSONUtil.toJSONIgnoreException(dbMsg));
+                } else {
+                    LOGGER.info("PushMsgServiceImpl doSavePushMsg error: push token is empty:appkey:{},device:{},msg:{}"
+                        + appkey, device.getDeviceid(), JSONUtil.toJSONIgnoreException(thePushmsg));
+                }
+            }
+            try {
+                saveResultList = pushMsgRepository.save(lstMsgs);
+            } catch (Exception e) {
+                throw new PushException(e.getMessage());
+            }
+        } else {
+            LOGGER.info("PushMsgServiceImpl doSavePushMsg info: device is empty,then no need send msg!");
+        }
+        return saveResultList;
     }
 }
