4324fed7555b499a5995293225729fc99d9e0dd8
Alpha Hinex <AlphaHinex@gmail.com>
2018-08-27 10:10:23 +0800

    [未完][develop][] Push refactor (#572)
    * Refactor PushVendorFactory slightly, try to better debug
    
    * Add test case and debug info
    
    * Add debug log

--- subprojects/pep-push/pep-push/src/main/java/com/proper/enterprise/platform/push/vendor/PushVendorFactory.java
+++ subprojects/pep-push/pep-push/src/main/java/com/proper/enterprise/platform/push/vendor/PushVendorFactory.java
@@ -1,102 +1,115 @@
 package com.proper.enterprise.platform.push.vendor;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import com.proper.enterprise.platform.core.exception.ErrMsgException;
+import com.proper.enterprise.platform.core.utils.JSONUtil;
 import com.proper.enterprise.platform.dfs.api.service.DFSService;
 import com.proper.enterprise.platform.file.api.File;
 import com.proper.enterprise.platform.file.repository.FileRepository;
-import com.proper.enterprise.platform.sys.i18n.I18NUtil;
+import com.proper.enterprise.platform.push.common.model.enums.PushDeviceType;
+import com.proper.enterprise.platform.push.common.model.enums.PushMode;
+import com.proper.enterprise.platform.push.config.PushGlobalInfo;
+import com.proper.enterprise.platform.push.repository.PushDeviceRepository;
+import com.proper.enterprise.platform.push.repository.PushMsgRepository;
+import com.proper.enterprise.platform.sys.i18n.I18NService;
 import org.nutz.mapl.Mapl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.proper.enterprise.platform.push.config.PushGlobalInfo;
-import com.proper.enterprise.platform.push.repository.PushDeviceRepository;
-import com.proper.enterprise.platform.push.repository.PushMsgRepository;
-import com.proper.enterprise.platform.push.common.model.enums.PushDeviceType;
-import com.proper.enterprise.platform.push.common.model.enums.PushMode;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 通过反射+工厂方法来应用“开放封闭原则”（对扩展开发，对修改封闭）
- *
- * @author shen
  */
 @Component
 public class PushVendorFactory {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(PushVendorFactory.class);
+
     @Autowired
-    PushGlobalInfo globalInfo;
+    private PushGlobalInfo globalInfo;
     @Autowired
-    PushMsgRepository msgRepo;
+    private PushMsgRepository msgRepo;
     @Autowired
-    PushDeviceRepository deviceRepo;
+    private PushDeviceRepository deviceRepo;
     @Autowired
     private FileRepository fileRepository;
-
     @Autowired
-    DFSService dsfService;
+    private DFSService dsfService;
+    @Autowired
+    private I18NService i18NService;
 
-    final Map<String, AbstractPushVendorService> mapPushVendorServices = new ConcurrentHashMap<String, AbstractPushVendorService>();
-    public static final String BASE_PACKAGE = PushVendorFactory.class.getPackage().getName();
+    private static final Map<String, AbstractPushVendorService> SERVICE_POOL = new ConcurrentHashMap<>();
+    private static final String BASE_PACKAGE = PushVendorFactory.class.getPackage().getName();
 
-    public synchronized AbstractPushVendorService getPushVendorService(String appkey, PushDeviceType devicetype,
+    public synchronized AbstractPushVendorService getPushVendorService(String appKey, PushDeviceType deviceType,
                                                                        PushMode pushMode) {
-        AbstractPushVendorService service = null;
-        String key = getKey(appkey, devicetype, pushMode);
-        service = mapPushVendorServices.get(key);
-        if (service == null) {
-            try {
-                String clazzName = BASE_PACKAGE + "." + devicetype + "." + pushMode + ".PushVendorServiceImpl";
-                Class clazz = Class.forName(clazzName);
-                service = (AbstractPushVendorService) clazz.newInstance();
-                service.setAppkey(appkey);
-                service.setDevicetype(devicetype);
-                service.setPushMode(pushMode);
-                service.setMsgRepo(msgRepo);
-                service.setDeviceRepo(deviceRepo);
-                service.setGlobalInfo(globalInfo);
-                Object pushParams = Mapl.cell(globalInfo.getPushConfigs(),
-                    appkey + ".device." + devicetype + "." + pushMode);
-                Object diplomaId = Mapl.cell(pushParams,
-                    "diplomaId");
-                if (diplomaId != null) {
-                    ((LinkedHashMap) pushParams).put("input", getInputStream(diplomaId.toString()));
-                }
-                service.setPushParams(pushParams);
-                mapPushVendorServices.put(key, service);
-            } catch (Exception ex) {
-                LOGGER.error(ex.getMessage(), ex);
-                service = null;
-            }
+        String key = composeKey(appKey, deviceType, pushMode);
+        if (SERVICE_POOL.containsKey(key)) {
+            LOGGER.debug("Current service pool has key set {}, contains {}", getKeySet(), key);
+            return SERVICE_POOL.get(key);
         }
-        return service;
+
+        try {
+            String clazzName = BASE_PACKAGE + "." + deviceType + "." + pushMode + ".PushVendorServiceImpl";
+            Class clazz = Class.forName(clazzName);
+            AbstractPushVendorService service = (AbstractPushVendorService) clazz.newInstance();
+            service.setAppkey(appKey);
+            service.setDevicetype(deviceType);
+            service.setPushMode(pushMode);
+            service.setMsgRepo(msgRepo);
+            service.setDeviceRepo(deviceRepo);
+            service.setGlobalInfo(globalInfo);
+            Object pushParams = Mapl.cell(globalInfo.getPushConfigs(),
+                appKey + ".device." + deviceType + "." + pushMode);
+            LOGGER.debug("Get pushParams of appKey({}), deviceType({}) and pushMode({}) is {}",
+                appKey, deviceType, pushMode, JSONUtil.toJSONIgnoreException(pushParams));
+            Object diplomaId = Mapl.cell(pushParams,
+                "diplomaId");
+            LOGGER.debug("DiplomaId of appKey({}), deviceType({}) and pushMode({}) is {}",
+                appKey, deviceType, pushMode, diplomaId);
+            if (diplomaId != null) {
+                ((LinkedHashMap) pushParams).put("input", getInputStream(diplomaId.toString()));
+            }
+            service.setPushParams(pushParams);
+            LOGGER.debug("Current service pool has key set {}, put service of {}", getKeySet(), key);
+            SERVICE_POOL.put(key, service);
+            return service;
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage(), ex);
+        }
+        return null;
     }
 
-    /**
-     * 生成发送推送对应的缓存的key，用于mapSendConnections
-     */
-    public String getKey(String appkey, PushDeviceType devicetype, PushMode pushMode) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(appkey).append("_").append(devicetype.toString()).append("_").append(pushMode);
-        return sb.toString();
+    private String getKeySet() {
+        return Arrays.toString(SERVICE_POOL.keySet().toArray());
     }
 
+    private String composeKey(String appKey, PushDeviceType deviceType, PushMode pushMode) {
+        return appKey + "_" + deviceType.toString() + "_" + pushMode;
+    }
 
-    private InputStream getInputStream(String fileId) throws IOException {
+    private InputStream getInputStream(String fileId) {
         File file = fileRepository.findOne(fileId);
         if (null == file) {
-            throw new ErrMsgException(I18NUtil.getMessage("pep.file.download.not.find"));
+            LOGGER.error("Could NOT find file entity info of {}", fileId);
+            throw new ErrMsgException(i18NService.getMessage("pep.file.download.not.find"));
         }
-        InputStream inputStream = dsfService.getFile(file.getFilePath());
-        if (inputStream == null) {
-            throw new ErrMsgException(I18NUtil.getMessage("pep.file.download.not.find"));
+        InputStream inputStream = null;
+        try {
+            inputStream = dsfService.getFile(file.getFilePath());
+            if (inputStream == null) {
+                LOGGER.error("Could NOT get input stream of {} from DFS service", file.getFilePath());
+                throw new ErrMsgException(i18NService.getMessage("pep.file.download.not.find"));
+            }
+        } catch (IOException ioe) {
+            LOGGER.error("Error occurs when getting input stream of file " + JSONUtil.toJSONIgnoreException(file), ioe);
         }
         return inputStream;
     }
