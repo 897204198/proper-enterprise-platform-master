b7345d17b06a61b7ba5fa1f37ac989009cfad45f
SuperMan <30485055+jianlinz@users.noreply.github.com>
2018-08-28 11:18:18 +0800

    delMongoDestory (#606)

--- subprojects/pep-core/pep-core-mongo/src/main/java/com/proper/enterprise/platform/core/mongo/client/PEPMongoClient.java
+++ subprojects/pep-core/pep-core-mongo/src/main/java/com/proper/enterprise/platform/core/mongo/client/PEPMongoClient.java
@@ -4,28 +4,16 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
-import com.proper.enterprise.platform.core.utils.StringUtil;
 import org.springframework.beans.factory.annotation.Value;
 
-import javax.annotation.PreDestroy;
 import java.util.List;
 
 public class PEPMongoClient extends MongoClient {
     @Value("${mongodb.database}")
     private String databaseName;
-    @Value("${mongodb.destory}")
-    private String destory;
-
-    private static final String DROP = "drop";
 
     public PEPMongoClient(final List<ServerAddress> seeds, final List<MongoCredential> credentialsList) {
         super(seeds, credentialsList, new MongoClientOptions.Builder().build());
     }
 
-    @PreDestroy
-    public void preDestory() {
-        if (StringUtil.isNotEmpty(destory) && DROP.equals(destory)) {
-            this.dropDatabase(databaseName);
-        }
-    }
 }
