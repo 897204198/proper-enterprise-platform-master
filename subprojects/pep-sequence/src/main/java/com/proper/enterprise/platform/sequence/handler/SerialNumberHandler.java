a685657f4fed0fd69dfde304b95ee76520401613
方明 <40154851+Kirinfm@users.noreply.github.com>
2018-09-07 13:20:31 +0800

    [完美][][develop] v0.5.x findbugs 空指针异常 (#642)
    修改流水号生成模块代码中潜在空指针异常的代码段，交由 Optional 进行处理

--- subprojects/pep-sequence/src/main/java/com/proper/enterprise/platform/sequence/handler/SerialNumberHandler.java
+++ subprojects/pep-sequence/src/main/java/com/proper/enterprise/platform/sequence/handler/SerialNumberHandler.java
@@ -1,10 +1,14 @@
 package com.proper.enterprise.platform.sequence.handler;
 
 import com.proper.enterprise.platform.api.cache.CacheDuration;
+import com.proper.enterprise.platform.sys.i18n.I18NUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.Optional;
 
 @Service
 @CacheDuration(cacheName = "pep.cache.serialNumberHandler")
@@ -26,13 +30,14 @@ public class SerialNumberHandler {
      * @return 自增值
      */
     public Long getNextID(String atomicName) {
-        Cache cache = cacheManager.getCache(CACHE_NAME);
-        Cache.ValueWrapper valueWrapper = cache.get(atomicName);
-        if (valueWrapper == null) {
+        Cache cache = getCache();
+        Optional<Cache.ValueWrapper> valueWrapper = Optional.ofNullable(cache.get(atomicName));
+        if (!valueWrapper.isPresent()) {
             cache.putIfAbsent(atomicName, 0L);
         }
         synchronized (SerialNumberHandler.class) {
-            Long object = (Long)cache.get(atomicName).get();
+            valueWrapper = Optional.ofNullable(cache.get(atomicName));
+            Long object =  Optional.ofNullable((Long) valueWrapper.get().get()).orElse(0L);
             cache.put(atomicName, object + 1);
             return object + 1;
         }
@@ -45,8 +50,13 @@ public class SerialNumberHandler {
      * @return 当前值
      */
     public Long getCurrentID(String atomicName) {
-        Cache cache = cacheManager.getCache(CACHE_NAME);
-        return cache.get(atomicName) == null ? 0L : Long.parseLong(cache.get(atomicName).get().toString());
+        Cache cache = getCache();
+        Optional<Cache.ValueWrapper> valueWrapper = Optional.ofNullable(cache.get(atomicName));
+        if (!valueWrapper.isPresent()) {
+            return 0L;
+        } else {
+            return Optional.ofNullable((Long) valueWrapper.get().get()).orElse(0L);
+        }
     }
 
     /**
@@ -56,7 +66,17 @@ public class SerialNumberHandler {
      * @param newValue 更新值
      */
     public void setCurrentId(String atomicName, Long newValue) {
-        Cache cache = cacheManager.getCache(CACHE_NAME);
+        Cache cache = getCache();
         cache.put(atomicName, newValue);
+    }
+
+    /**
+     * 获取流水号的缓存
+     *
+     * @return Cache
+     */
+    private Cache getCache() {
+        Assert.notNull(cacheManager.getCache(CACHE_NAME), I18NUtil.getMessage("sequence.cache.get.fail"));
+        return cacheManager.getCache(CACHE_NAME);
     }
 }
