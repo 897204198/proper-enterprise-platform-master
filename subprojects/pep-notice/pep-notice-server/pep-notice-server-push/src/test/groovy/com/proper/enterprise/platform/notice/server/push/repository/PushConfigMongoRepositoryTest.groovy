2fca13e3496e3454cb890b9a8a4b02ca1d4dc077
zhangjianlin <jianlin1215@outlook.com>
2018-09-11 16:36:36 +0800

    推送重构IOS

---
+++ subprojects/pep-notice/pep-notice-server/pep-notice-server-push/src/test/groovy/com/proper/enterprise/platform/notice/server/push/repository/PushConfigMongoRepositoryTest.groovy
@@ -0,0 +1,35 @@
+package com.proper.enterprise.platform.notice.server.push.repository
+
+import com.proper.enterprise.platform.notice.server.push.dao.document.PushConfDocument
+import com.proper.enterprise.platform.notice.server.push.dao.repository.PushConfigMongoRepository
+import com.proper.enterprise.platform.notice.server.push.enums.PushChannelEnum
+import com.proper.enterprise.platform.test.AbstractTest
+import com.proper.enterprise.platform.test.annotation.NoTx
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.DuplicateKeyException
+
+class PushConfigMongoRepositoryTest extends AbstractTest {
+
+    @Autowired
+    PushConfigMongoRepository pushConfigMongoRepository
+
+    @Test
+    public void testUniqueIndex() {
+        PushConfDocument pushConf = new PushConfDocument()
+        pushConf.setAppKey("testPushConfSave")
+        pushConf.setPushPackage("testPushConfSave")
+        pushConf.setPushChannel(PushChannelEnum.IOS)
+        pushConfigMongoRepository.save(pushConf)
+        PushConfDocument pushConf2 = new PushConfDocument()
+        pushConf2.setAppKey("testPushConfSave")
+        pushConf2.setPushPackage("testPushConfSave")
+        pushConf2.setPushChannel(PushChannelEnum.IOS)
+        try {
+            pushConfigMongoRepository.save(pushConf2)
+        } catch (DuplicateKeyException e) {
+            print "valid success"
+        }
+
+    }
+}
