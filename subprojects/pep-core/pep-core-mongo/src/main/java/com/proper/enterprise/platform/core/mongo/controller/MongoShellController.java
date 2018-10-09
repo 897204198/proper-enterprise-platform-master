0fc195aab9ca4e775894f80e3d8d091c01d4272e
wyiyi <1084896032@qq.com>
2018-10-09 13:55:58 +0800

    [完成][0.5.x][pep-1326] /File && /msc 模块 swagger (#704)

--- subprojects/pep-core/pep-core-mongo/src/main/java/com/proper/enterprise/platform/core/mongo/controller/MongoShellController.java
+++ subprojects/pep-core/pep-core-mongo/src/main/java/com/proper/enterprise/platform/core/mongo/controller/MongoShellController.java
@@ -5,8 +5,12 @@ import com.proper.enterprise.platform.core.controller.BaseController;
 import com.proper.enterprise.platform.core.entity.DataTrunk;
 import com.proper.enterprise.platform.core.mongo.service.MongoShellService;
 import com.proper.enterprise.platform.core.utils.StringUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/msc")
+@Api(tags = "/msc")
 public class MongoShellController extends BaseController {
 
     private static final String COMMA = ",";
@@ -25,19 +30,25 @@ public class MongoShellController extends BaseController {
     private MongoShellService service;
 
     @PostMapping("/{collection}")
-    public ResponseEntity<Document> create(@PathVariable String collection, @RequestBody String document) {
+    @ApiOperation("‍保存mongo的数据")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Document> create(@ApiParam(value = "‍表名", required = true) @PathVariable String collection, @RequestBody String document) {
         return responseOfPost(service.insertOne(collection, document));
     }
 
     @GetMapping("/{collection}/{id}")
-    public ResponseEntity<Document> get(@PathVariable String collection, @PathVariable String id) throws Exception {
+    @ApiOperation("‍根据id获取对应表的数据")
+    public ResponseEntity<Document> get(@ApiParam(value = "‍表名", required = true) @PathVariable String collection,
+                                        @ApiParam(value = "‍表对应的id", required = true) @PathVariable String id) throws Exception {
         Document doc = service.queryById(collection, id);
         return responseOfGet(doc);
     }
 
     @GetMapping("/{collection}")
-    public ResponseEntity<DataTrunk<Document>> find(@PathVariable String collection,
-                                                    @RequestParam String query, String limit, String sort) throws Exception {
+    @ApiOperation("‍获取mongo的数据，参考文档：https://docs.mongodb.com/manual/tutorial/query-documents/")
+    public ResponseEntity<DataTrunk<Document>> find(@ApiParam(value = "‍表名", required = true) @PathVariable String collection,
+                                                    @ApiParam(value = "‍查询条件", required = true) @RequestParam String query,
+                                                    String limit, String sort) throws Exception {
         query = decodeUrl(query);
         List<Document> docs = service.query(collection, query,
             StringUtil.isNumeric(limit) ? Integer.parseInt(limit) : -1,
@@ -53,7 +64,10 @@ public class MongoShellController extends BaseController {
     }
 
     @DeleteMapping("/{collection}/{ids}")
-    public ResponseEntity deleteByIds(@PathVariable String collection, @PathVariable String ids) throws Exception {
+    @ApiOperation("‍删除ID对应表的信息")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity deleteByIds(@ApiParam(value = "‍表名", required = true) @PathVariable String collection,
+                                      @ApiParam(value = "‍id列表(使用\",\"分割)", required = true) @PathVariable String ids) throws Exception {
         boolean exist;
         if (ids.contains(COMMA)) {
             List<Document> docs = service.deleteByIds(collection, ids.split(","));
@@ -66,8 +80,10 @@ public class MongoShellController extends BaseController {
     }
 
     @PutMapping("/{collection}/{id}")
-    public ResponseEntity<Document> update(@PathVariable String collection,
-                                           @PathVariable String id, @RequestBody String update) throws Exception {
+    @ApiOperation("‍根据id修改对应表的数据")
+    public ResponseEntity<Document> update(@ApiParam(value = "‍表名", required = true) @PathVariable String collection,
+                                           @ApiParam(value = "‍表对应的id", required = true) @PathVariable String id,
+                                           @RequestBody String update) throws Exception {
         return responseOfPut(service.updateById(collection, id, update));
     }
 
--- subprojects/pep-file/pep-file/src/main/java/com/proper/enterprise/platform/file/controller/FileController.java
+++ subprojects/pep-file/pep-file/src/main/java/com/proper/enterprise/platform/file/controller/FileController.java
@@ -4,7 +4,10 @@ import com.proper.enterprise.platform.core.controller.BaseController;
 import com.proper.enterprise.platform.core.entity.DataTrunk;
 import com.proper.enterprise.platform.file.service.FileService;
 import com.proper.enterprise.platform.file.vo.FileVO;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,6 +18,7 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/file")
+@Api(tags = "/file")
 public class FileController extends BaseController {
 
     private FileService fileService;
@@ -25,34 +29,50 @@ public class FileController extends BaseController {
     }
 
     @PostMapping
+    @ApiOperation(value = "‍上传文件", produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<String> uploadFile(MultipartFile file) throws IOException {
         return responseOfPost(fileService.save(file).getId());
     }
 
     @PostMapping(path = "/{id}")
-    public ResponseEntity<String> updateFile(@PathVariable String id, MultipartFile file) throws IOException {
+    @ApiOperation(value = "‍更新文件", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> updateFile(@ApiParam(value = "‍文件id", required = true) @PathVariable String id, MultipartFile
+            file) throws IOException {
         return responseOfPost(fileService.update(id, file).getId());
     }
 
 
     @GetMapping(path = "/{id}")
-    public void download(@PathVariable String id, HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @ApiOperation(value = "‍根据id获取文件", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public void download(@ApiParam(value = "‍文件id", required = true) @PathVariable String id, HttpServletRequest request, HttpServletResponse
+            response) throws IOException {
         fileService.download(id, request, response);
     }
 
     @GetMapping(path = "/{id}/meta")
-    public ResponseEntity<FileVO> metainfo(@PathVariable String id) {
+    @ApiOperation("‍根据id获取元文件")
+    public ResponseEntity<FileVO> metainfo(@ApiParam(value = "‍文件id", required = true) @PathVariable String id) {
         return responseOfGet(fileService.findById(id), FileVO.class);
     }
 
     @DeleteMapping
-    public ResponseEntity delete(@RequestParam String ids) throws IOException {
+    @ApiOperation("‍删除多个文件")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity delete(@ApiParam(value = "‍文件信息Id列表(使用\",\"分割)", required = true) @RequestParam String ids) throws IOException {
         return responseOfDelete(fileService.deleteByIds(ids));
     }
 
     @GetMapping
+    @ApiOperation("‍获取所有文件")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "pageNo", value = "‍页码", required = true, paramType = "query", dataType = "int"),
+            @ApiImplicitParam(name = "pageSize", value = "‍每页条数", required = true, paramType = "query", dataType = "int")
+    })
     public ResponseEntity<DataTrunk<FileVO>> query() {
         return responseOfGet(fileService.findPage(), FileVO.class);
     }
+
 
 }
