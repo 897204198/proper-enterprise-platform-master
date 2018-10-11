a8de08c5d7dfe243e1664e4e52866035eef056cc
SuperMan <30485055+jianlinz@users.noreply.github.com>
2018-10-11 13:31:26 +0800

    [完美][develop]修改oopsearch同步mysql变更的bug(列名大小写) (#722)
    oopsearch在同步mysql的时候 用表名+列名作为唯一标识
    表名我们已经忽略大小写了 但是列名有大写有小写 存在问题
    我们现在将表名和列名在匹配的时候统一转成小写做匹配

--- subprojects/pep-oopsearch/pep-oopsearch-config/src/main/java/com/proper/enterprise/platform/oopsearch/config/conf/AbstractSearchConfigs.java
+++ subprojects/pep-oopsearch/pep-oopsearch-config/src/main/java/com/proper/enterprise/platform/oopsearch/config/conf/AbstractSearchConfigs.java
@@ -12,7 +12,7 @@ import java.util.*;
 /**
  * oopsearch组件查询配置类父类
  * 每一个使用oopsearch组件的业务模块，需配置一个查询配置类，继承该类
- * */
+ */
 public abstract class AbstractSearchConfigs {
 
     /**
@@ -79,13 +79,14 @@ public abstract class AbstractSearchConfigs {
 
     /**
      * 默认构造函数
-     * @param  searchTables 查询使用的表
-     * @param  searchColumns 查询使用的字段
-     * @param  limit 返回结果最大条数
-     * @param  extendByYear 对于年份，输入内容，可以被识别的内容
-     * @param  extendByMonth 对于月份，输入内容，可以被识别的内容
-     * @param  extendByDay 对于天，输入内容，可以被识别的内容
-     * */
+     *
+     * @param searchTables  查询使用的表
+     * @param searchColumns 查询使用的字段
+     * @param limit         返回结果最大条数
+     * @param extendByYear  对于年份，输入内容，可以被识别的内容
+     * @param extendByMonth 对于月份，输入内容，可以被识别的内容
+     * @param extendByDay   对于天，输入内容，可以被识别的内容
+     */
     public AbstractSearchConfigs(String searchTables,
                                  String searchColumns,
                                  int limit,
@@ -105,7 +106,7 @@ public abstract class AbstractSearchConfigs {
 
     /**
      * 初始化各变量
-     * */
+     */
     private void init() {
         String[] tableNameArr = searchTables.split(",");
         tableNameList = Arrays.asList(tableNameArr);
@@ -139,16 +140,16 @@ public abstract class AbstractSearchConfigs {
 
     /**
      * 根据字段的类型（string、date、int）获取对应的字段集合
-     * @param  type 字段类型（string、date、int）
      *
+     * @param type 字段类型（string、date、int）
      * @return 查询字段集合
-     * */
+     */
     public List<SearchColumnModel> getSearchColumnListByType(String type) {
         List<SearchColumnModel> searchColumnList = new ArrayList<>();
         List<String> tableNameList = this.getTableNameList();
-        for (String tableName: tableNameList) {
+        for (String tableName : tableNameList) {
             List<SearchColumnModel> searchColumnTempList = searchTableColumnMap.get(tableName);
-            for (SearchColumnModel searchColumn: searchColumnTempList) {
+            for (SearchColumnModel searchColumn : searchColumnTempList) {
                 if (type.equalsIgnoreCase(searchColumn.getType())) {
                     searchColumnList.add(searchColumn);
                 }
@@ -161,12 +162,12 @@ public abstract class AbstractSearchConfigs {
      * 获取查询字段与表名对应的集合(获取的是searchTableColumnMap变量的拷贝)
      *
      * @return 查询字段对象与表名对应的集合
-     * */
+     */
     @SuppressWarnings("unchecked")
     public Map<String, List<SearchColumnModel>> getSearchTableColumnMap() {
         Map<String, List<SearchColumnModel>> copySearchTableColumnMap = new HashMap<>(searchTableColumnMap.size());
         try {
-            copySearchTableColumnMap = SerializationUtil.deepClone((HashMap)searchTableColumnMap);
+            copySearchTableColumnMap = SerializationUtil.deepClone((HashMap) searchTableColumnMap);
         } catch (IOException e) {
             LOGGER.error("invoke method getSearchTableColumnMap fail", e);
         }
@@ -182,8 +183,8 @@ public abstract class AbstractSearchConfigs {
         Assert.notNull(columnName, "COULD NOT GET DESC FROM NULL COLUMNNAME!");
         Map<String, SearchColumnModel> map = searchTableColumn.get(tableName.toLowerCase());
         if (map != null) {
-            SearchColumnModel model = map.get(columnName);
-            return model;
+            //列忽略大小写
+            return map.get(columnName.toLowerCase());
         } else {
             return null;
         }
@@ -193,7 +194,7 @@ public abstract class AbstractSearchConfigs {
      * 获取查询使用的表名集合
      *
      * @return 表名集合
-     * */
+     */
     public List<String> getTableNameList() {
         return tableNameList;
     }
@@ -202,7 +203,7 @@ public abstract class AbstractSearchConfigs {
      * 通过表名获取查询字段对象
      *
      * @return 查询字段对象的集合
-     * */
+     */
     public List<SearchColumnModel> getSearchColumnListByTable(String tableName) {
         return searchTableColumnMap.get(tableName.toLowerCase());
     }
@@ -211,7 +212,7 @@ public abstract class AbstractSearchConfigs {
      * 获取查询最大条数
      *
      * @return 最大条数
-     * */
+     */
     public int getLimit() {
         return limit;
     }
@@ -221,7 +222,7 @@ public abstract class AbstractSearchConfigs {
      * 获取对于年份，输入内容，可以被识别的内容数组
      *
      * @return 年份内容识别数组
-     * */
+     */
     public final String[] getExtendByYearArr() {
         if (extendByYearArr != null) {
             return extendByYearArr.clone();
@@ -234,7 +235,7 @@ public abstract class AbstractSearchConfigs {
      * 获取对于月份，输入内容，可以被识别的内容数组
      *
      * @return 月份内容识别数组
-     * */
+     */
     public String[] getExtendByMonthArr() {
         if (extendByMonthArr != null) {
             return extendByMonthArr.clone();
@@ -247,7 +248,7 @@ public abstract class AbstractSearchConfigs {
      * 获取对于天，输入内容，可以被识别的内容数组
      *
      * @return 天内容识别数组
-     * */
+     */
     public String[] getExtendByDayArr() {
         if (extendByDayArr != null) {
             return extendByDayArr.clone();
