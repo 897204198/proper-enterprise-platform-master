fac4b647a6e7e5bbf7d818bdeb1ec8c32261d030
方明 <40154851+Kirinfm@users.noreply.github.com>
2018-10-24 09:49:11 +0800

    [完成][][develop] 增加待办接口表单数据 (#750)
    增加待办接口表单数据
    前台待办需要显示表单中的内容

--- subprojects/pep-workflow/src/main/java/com/proper/enterprise/platform/workflow/service/impl/PEPTaskServiceImpl.java
+++ subprojects/pep-workflow/src/main/java/com/proper/enterprise/platform/workflow/service/impl/PEPTaskServiceImpl.java
@@ -184,6 +184,7 @@ public class PEPTaskServiceImpl implements PEPTaskService {
     public DataTrunk<PEPTaskVO> findTaskAssigneeIsMePagination(String processDefinitionName, PageRequest pageRequest) {
         HistoricTaskInstanceQuery historicTaskInstanceQuery = historyService.createHistoricTaskInstanceQuery()
             .includeProcessVariables()
+            .includeTaskLocalVariables()
             .taskAssignee(Authentication.getCurrentUserId())
             .finished()
             .orderByHistoricTaskInstanceEndTime()
