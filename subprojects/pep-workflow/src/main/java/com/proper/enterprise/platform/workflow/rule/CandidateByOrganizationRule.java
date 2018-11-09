b168c77e37fe535406aeb982706716d5b821831c
方明 <40154851+Kirinfm@users.noreply.github.com>
2018-11-09 14:39:07 +0800

    [完成][][develop] 增加根据流程执行实例获取待办人/组/角色 (#779)
    # 增加根据流程执行实例获取待办人/组/角色
    由于获取`待办人/组/角色`可能会根据流程中的其他变量进行获取/筛选，故增加传入流程执行实例变量。
    实现该方法，可以获取流程中的所有变量进行业务实现

--- subprojects/pep-workflow/src/main/java/com/proper/enterprise/platform/workflow/rule/CandidateByOrganizationRule.java
+++ subprojects/pep-workflow/src/main/java/com/proper/enterprise/platform/workflow/rule/CandidateByOrganizationRule.java
@@ -1,5 +1,7 @@
 package com.proper.enterprise.platform.workflow.rule;
 
+import org.flowable.engine.delegate.DelegateExecution;
+
 import java.util.List;
 
 public interface CandidateByOrganizationRule {
@@ -11,4 +13,12 @@ public interface CandidateByOrganizationRule {
      * @return 候选经办 人/用户组/角色 集合
      */
     List<String> execute(String organization);
+
+    /**
+     * 设定候选经办 人/用户组/角色 集合
+     *
+     * @param delegateExecution 流程执行实例
+     * @return 候选经办 人/用户组/角色 集合
+     */
+    List<String> executeByExecution(DelegateExecution delegateExecution);
 }
