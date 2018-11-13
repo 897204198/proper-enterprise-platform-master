5282f5f77d3218e6b4d4a570f8c02bd9f4bb706c
SuperMan <30485055+jianlinz@users.noreply.github.com>
2018-11-13 16:10:35 +0800

    [完美][develop]华为推送url跳转 (#790)
    华为推送url跳转
    自定义map中传入url则当做uri做跳转
    将小米推送改为 我的token
    在检验后返回给客户端的异常添加日志打印出来便于排查没记录到服务端的推送数据

--- subprojects/pep-notice/pep-notice-server/pep-notice-server-push/src/test/groovy/com/proper/enterprise/platform/notice/server/push/constant/XiaomiConstant.groovy
+++ subprojects/pep-notice/pep-notice-server/pep-notice-server-push/src/test/groovy/com/proper/enterprise/platform/notice/server/push/constant/XiaomiConstant.groovy
@@ -5,5 +5,5 @@ class XiaomiConstant {
     public static final String APPKEY = "MobileOADev"
     public static final String APPSECRET = "RGW+NA+T2ucpEX0a6bxyhA=="
     public static final String PACKAGENAME = "com.proper.icmp.dev"
-    public static final String PUSHTOKEN = "o4gN1LuTsk6/CM7TKf0tTbj2MIWimxTGxRo8yZFQTJAhNsGlEeZLbMIeYnZ9BshJ"
+    public static final String PUSHTOKEN = "osEbkApvY/uPiNNtfXOKzyF+Pw64KiQ3WCRhh4ZEcVbwmNBbVG9sGjtZ0SKuzBFF"
 }
