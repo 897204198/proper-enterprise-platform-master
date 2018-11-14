0da28f1d41fa92a35984c33d8caecaa109c41a93
Alpha Hinex <AlphaHinex@gmail.com>
2018-11-14 08:45:43 +0800

    Make checkstyle happy

--- subprojects/fundamental/pep-workflow/src/test/groovy/com/proper/enterprise/platform/workflow/frame/service/FormPropertyService.java
+++ subprojects/fundamental/pep-workflow/src/test/groovy/com/proper/enterprise/platform/workflow/frame/service/FormPropertyService.java
@@ -1,7 +1,6 @@
 package com.proper.enterprise.platform.workflow.frame.service;
 
 import com.proper.enterprise.platform.core.CoreProperties;
-import com.proper.enterprise.platform.core.PEPConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
