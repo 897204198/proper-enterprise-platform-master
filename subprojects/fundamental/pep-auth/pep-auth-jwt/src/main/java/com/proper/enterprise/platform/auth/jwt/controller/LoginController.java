c25186e8809141a24e1525c7c8d778e5984fa219
Alpha Hinex <AlphaHinex@gmail.com>
2018-11-20 14:28:55 +0800

    Fix a bug

--- subprojects/fundamental/pep-auth/pep-auth-jwt/src/main/java/com/proper/enterprise/platform/auth/jwt/controller/LoginController.java
+++ subprojects/fundamental/pep-auth/pep-auth-jwt/src/main/java/com/proper/enterprise/platform/auth/jwt/controller/LoginController.java
@@ -27,6 +27,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
