2dbe856b4cc48bd6f56148cdbf3e3ae26c670047
Alpha Hinex <AlphaHinex@gmail.com>
2019-01-02 17:14:39 +0800

    Fix a bug

--- subprojects/fundamental/pep-core/pep-core/src/test/groovy/com/proper/enterprise/platform/core/utils/AntResourceUtilSpec.groovy
+++ subprojects/fundamental/pep-core/pep-core/src/test/groovy/com/proper/enterprise/platform/core/utils/AntResourceUtilSpec.groovy
@@ -5,7 +5,7 @@ import spock.lang.Specification
 class AntResourceUtilSpec extends Specification {
 
     def "Support multi-locations separated by comma"() {
-        def res = AntResourceUtil.getResources('classpath*:com/proper/**/*.p12,classpath*:banner.txt')
+        def res = AntResourceUtil.getResources('classpath*:com/proper/**/*.p12,classpath*:com/proper/**/cer*')
 
         expect:
         res.length == 2 // According to actual files under path pattern
