4ba6bfe14a0296a2d37b29099f04ac0a21fea8d4
Alpha Hinex <AlphaHinex@gmail.com>
2019-01-10 15:10:29 +0800

    Little change

--- subprojects/fundamental/pep-websocket/src/main/java/com/proper/enterprise/platform/websocket/test/controller/TestServiceSendController.java
+++ subprojects/fundamental/pep-websocket/src/main/java/com/proper/enterprise/platform/websocket/test/controller/TestServiceSendController.java
@@ -1,27 +1,37 @@
 package com.proper.enterprise.platform.websocket.test.controller;
 
 import com.proper.enterprise.platform.core.controller.BaseController;
+import com.proper.enterprise.platform.core.utils.DateUtil;
+import com.proper.enterprise.platform.core.utils.StringUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @Api("testServiceSend")
 public class TestServiceSendController extends BaseController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestServiceSendController.class);
 
     @Autowired
     private SimpMessagingTemplate template;
 
     @GetMapping("/test/socket/sendAll")
     @ApiOperation("sendAll")
-    public ResponseEntity<String> sendAll() {
-        template.convertAndSend("/topic/all", "all");
-        return responseOfGet(null);
+    public String sendAll(String payload, HttpServletRequest request) {
+        LOGGER.info("Received {}", request);
+        template.convertAndSend("/topic/all", StringUtil.isBlank(payload) ? "all" : payload);
+        return "Broadcast at " + DateUtil.getTimestamp();
     }
 
     @GetMapping("/test/socket/send/{userId}")
