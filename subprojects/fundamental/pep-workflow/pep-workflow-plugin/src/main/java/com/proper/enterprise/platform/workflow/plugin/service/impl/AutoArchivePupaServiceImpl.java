262e54e27f9d789f0d323bd7195244eddfefe7f9
SuperMan <30485055+jianlinz@users.noreply.github.com>
2019-05-14 15:33:44 +0800

    [FEAT] pupa自动归档 (#1032)
    * pupa自动归档
    
    * 自动归档
    
    * 补充单元测试

--- subprojects/fundamental/pep-workflow/pep-workflow-plugin/src/main/java/com/proper/enterprise/platform/workflow/plugin/service/impl/AutoArchivePupaServiceImpl.java
+++ subprojects/fundamental/pep-workflow/pep-workflow-plugin/src/main/java/com/proper/enterprise/platform/workflow/plugin/service/impl/AutoArchivePupaServiceImpl.java
@@ -1,29 +1,73 @@
 package com.proper.enterprise.platform.workflow.plugin.service.impl;
 
+import com.proper.enterprise.platform.core.exception.ErrMsgException;
+import com.proper.enterprise.platform.core.mongo.constants.MongoConstants;
+import com.proper.enterprise.platform.core.mongo.dao.MongoDAO;
+import com.proper.enterprise.platform.core.security.Authentication;
+import com.proper.enterprise.platform.core.utils.CollectionUtil;
+import com.proper.enterprise.platform.core.utils.DateUtil;
 import com.proper.enterprise.platform.core.utils.JSONUtil;
-import com.proper.enterprise.platform.websocket.util.StompMessageSendUtil;
+import com.proper.enterprise.platform.core.utils.StringUtil;
 import com.proper.enterprise.platform.workflow.plugin.service.AutoArchiveService;
+import org.bson.Document;
 import org.flowable.engine.impl.persistence.entity.ExecutionEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service(value = "autoArchivePupa")
 public class AutoArchivePupaServiceImpl implements AutoArchiveService {
 
-    private static final String PROCINSTID = "procInstId";
+    @Autowired
+    private MongoDAO mongoDAO;
 
-    private static final String PROCESS_DEFINITION_ID = "processDefinitionId";
+    private static final String CONFIG_COLLECTION_NAME = "PEP_DEVTOOLS_CUSTOMQUERY";
 
-    private static final String PROCESS_DEFINITION_KEY = "processDefinitionKey ";
+    private static final String TABLE_NAME_KEY = "tableName";
 
     @Override
     public void archive(ExecutionEntity execution, String... forms) {
-        Map<String, String> archiveData = new HashMap<>(16);
-        archiveData.put(PROCINSTID, execution.getProcessInstanceId());
-        archiveData.put(PROCESS_DEFINITION_ID, execution.getProcessDefinitionId());
-        archiveData.put(PROCESS_DEFINITION_KEY, execution.getProcessDefinitionKey());
-        StompMessageSendUtil.send("/topic/autoArchive", JSONUtil.toJSONIgnoreException(archiveData));
+        if (null == forms) {
+            return;
+        }
+        for (String form : forms) {
+            try {
+                insert(execution, form);
+            } catch (Exception e) {
+                throw new ErrMsgException("Pupa Archive Error" + e.getMessage());
+            }
+        }
+    }
+
+    private String getTableName(String form) throws Exception {
+        String code = form.replace("@", "");
+        List<Document> configs = mongoDAO.query(CONFIG_COLLECTION_NAME, "{code:'" + code + "'}");
+        if (CollectionUtil.isEmpty(configs)) {
+            throw new ErrMsgException("Pupa Archive Error,cant find config by code,code is" + code);
+        }
+        if (configs.size() > 1) {
+            throw new ErrMsgException("Pupa Archive Error, find More than one config by code,code is" + code);
+        }
+        Document config = configs.get(0);
+        String tableName = (String) config.get(TABLE_NAME_KEY);
+        if (StringUtil.isEmpty(tableName)) {
+            throw new ErrMsgException("Pupa Archive Error, tableName is Empty,code is" + code);
+        }
+        return tableName;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void insert(ExecutionEntity execution, String form) throws Exception {
+        String tableName = getTableName(form);
+        Map<String, Object> formData = (Map<String, Object>) execution.getVariable(form);
+        if (null != formData) {
+            formData.put(MongoConstants.CREATE_TIME, DateUtil.getTimestamp());
+            formData.put(MongoConstants.LAST_MODIFY_TIME, DateUtil.getTimestamp());
+            formData.put(MongoConstants.CREATE_USER_ID, Authentication.getCurrentUserId());
+            formData.put(MongoConstants.LAST_MODIFY_USER_ID, Authentication.getCurrentUserId());
+            mongoDAO.insertOne(tableName, JSONUtil.toJSONIgnoreException(formData));
+        }
     }
 }
--- subprojects/fundamental/pep-workflow/pep-workflow-plugin/src/test/groovy/com/proper/enterprise/platform/workflow/api/notice/PupaAutoArchiveTest.groovy
+++ subprojects/fundamental/pep-workflow/pep-workflow-plugin/src/test/groovy/com/proper/enterprise/platform/workflow/api/notice/PupaAutoArchiveTest.groovy
@@ -1,72 +1,53 @@
 package com.proper.enterprise.platform.workflow.api.notice
 
-import com.proper.enterprise.platform.api.auth.service.AccessTokenService
-import com.proper.enterprise.platform.test.AbstractIntegrationTest
-import com.proper.enterprise.platform.test.utils.JSONUtil
-import com.proper.enterprise.platform.websocket.client.stomp.StompClient
-import com.proper.enterprise.platform.websocket.client.stomp.StompFrameStringHandler
+import com.proper.enterprise.platform.core.mongo.dao.MongoDAO
 import com.proper.enterprise.platform.workflow.vo.PEPProcInstVO
-import org.flowable.engine.HistoryService
-import org.flowable.engine.IdentityService
-import org.junit.Ignore
+import org.bson.Document
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
-import org.springframework.lang.Nullable
-import org.springframework.messaging.simp.stomp.StompHeaders
 import org.springframework.test.context.jdbc.Sql
 
-import java.util.concurrent.CountDownLatch
+class PupaAutoArchiveTest extends WorkflowAbstractTest {
 
-@Ignore
-class PupaAutoArchiveTest extends AbstractIntegrationTest {
-
-    @Autowired
-    IdentityService identityService
+    private static final String AUTO_ARCHIVETEST_KEY = "gd"
 
     @Autowired
-    HistoryService historyService
-
-    @Autowired
-    private AccessTokenService tokenService
-
-    def url, token
-
-    private static final String PUPA_AUTO_ARCHIVE_KEY = 'pupaAutoArchive'
-
+    private MongoDAO mongoDAO
 
     @Test
     @Sql(["/com/proper/enterprise/platform/workflow/identity.sql",
         "/com/proper/enterprise/platform/workflow/datadics.sql", "/com/proper/enterprise/platform/workflow/wfIdmQueryConf.sql"])
-    void pupaAutoArchive() {
-        token = post('/auth/login', """{"username":"testuser3","pwd":"123456"}""", HttpStatus.OK).getInputStream().getText()
-        url = "ws://localhost:$port/stomp?access_token=$token"
-        def stompUser = 'u1'
-        def client = StompClient.connect(stompUser, url)
-        CountDownLatch latch = new CountDownLatch(1)
-        client.subscribe("/topic/autoArchive", new StompFrameStringHandler() {
-            @Override
-            void handleFrame(StompHeaders headers, @Nullable Object payload) {
-                Map result = JSONUtil.parse(payload, Map.class)
-                if (result.get("procInstId")) {
-                    latch.countDown()
-                }
-            }
-        })
-        identityService.setAuthenticatedUserId('user3')
-        Map form1 = new HashMap()
-        form1.put("a", "a")
-        PEPProcInstVO pepProcInstVO = start(PUPA_AUTO_ARCHIVE_KEY, form1)
+    public void autoArchive() {
+        initPupaConfig()
+        identityService.setAuthenticatedUserId('user1')
+        Map data = new HashMap()
+        data.put("test", "test")
+        PEPProcInstVO pepProcInstVO = start(AUTO_ARCHIVETEST_KEY, data)
         assert isEnded(pepProcInstVO.getProcInstId())
-        Thread.sleep(100000)
+        List<Document> archiveData = mongoDAO.query("PEP_PUPA_GD", "{test:'test'}")
+        assert archiveData.size() == 1
     }
 
-    boolean isEnded(String procInstId) {
-        return null != historyService.createHistoricProcessInstanceQuery().processInstanceId(procInstId).finished().singleResult()
-    }
+    private void initPupaConfig() {
+        mongoDAO.insertOne("PEP_DEVTOOLS_CUSTOMQUERY", "{\n" +
+            "  \"id\" : \"\",\n" +
+            "  \"functionName\" : \"测试归档\",\n" +
+            "  \"tableName\" : \"PEP_PUPA_GD\",\n" +
+            "  \"code\" : \"gd\",\n" +
+            "  \"note\" : \"\",\n" +
+            "  \"enable\" : true,\n" +
+            "  \"_ClientVersion\" : \"js0.6.4-proper\",\n" +
+            "  \"_InstallationId\" : \"035c3dc2-df17-d8cc-1867-283cff376e79\",\n" +
+            "  \"CT\" : \"2019-05-14 11:19:12\",\n" +
+            "  \"LT\" : \"2019-05-14 11:55:51\",\n" +
+            "  \"CU\" : \"pep-sysadmin\",\n" +
+            "  \"LU\" : \"pep-sysadmin\",\n" +
+            "  \"formConfig\" : \"{\\\"formLayout\\\":\\\"horizontal\\\",\\\"formJson\\\":[{\\\"label\\\":\\\"输入框\\\",\\\"key\\\":\\\"Input\\\",\\\"component\\\":{\\\"name\\\":\\\"Input\\\"},\\\"name\\\":\\\"test\\\",\\\"active\\\":false}]}\",\n" +
+            "  \"gridConfig\" : \"{\\\"columns\\\":[{\\\"_id\\\":\\\"tq2vollcld8\\\",\\\"title\\\":\\\"输入框\\\",\\\"dataIndex\\\":\\\"qqq\\\"}],\\\"topButtons\\\":[{\\\"_id\\\":\\\"1557806151054.9058\\\",\\\"text\\\":\\\"发起\\\",\\\"name\\\":\\\"start\\\",\\\"position\\\":\\\"top\\\",\\\"type\\\":\\\"primary\\\",\\\"icon\\\":\\\"plus\\\",\\\"enable\\\":true,\\\"default\\\":true},{\\\"_id\\\":\\\"1557803983844.955\\\",\\\"text\\\":\\\"新建\\\",\\\"name\\\":\\\"create\\\",\\\"position\\\":\\\"top\\\",\\\"type\\\":\\\"primary\\\",\\\"icon\\\":\\\"plus\\\",\\\"enable\\\":true,\\\"default\\\":true},{\\\"_id\\\":\\\"1557803983844.6018\\\",\\\"text\\\":\\\"删除\\\",\\\"name\\\":\\\"batchDelete\\\",\\\"position\\\":\\\"top\\\",\\\"type\\\":\\\"default\\\",\\\"icon\\\":\\\"delete\\\",\\\"display\\\":\\\"items=>(items.length > 0)\\\",\\\"enable\\\":true,\\\"default\\\":true}],\\\"rowButtons\\\":[{\\\"_id\\\":\\\"1557803983844.8403\\\",\\\"text\\\":\\\"编辑\\\",\\\"name\\\":\\\"edit\\\",\\\"position\\\":\\\"row\\\",\\\"icon\\\":\\\"edit\\\",\\\"type\\\":\\\"default\\\",\\\"enable\\\":true,\\\"default\\\":true},{\\\"_id\\\":\\\"1557803983844.7983\\\",\\\"text\\\":\\\"删除\\\",\\\"name\\\":\\\"delete\\\",\\\"position\\\":\\\"row\\\",\\\"icon\\\":\\\"delete\\\",\\\"type\\\":\\\"default\\\",\\\"enable\\\":true,\\\"default\\\":true}]}\",\n" +
+            "  \"modalConfig\" : \"{\\\"title\\\":\\\"\\\",\\\"width\\\":1000,\\\"footer\\\":[\\\"submit\\\",\\\"cancel\\\"],\\\"saveAfterClosable\\\":true,\\\"maskClosable\\\":false}\",\n" +
+            "  \"relaWf\" : true,\n" +
+            "  \"wfKey\" : \"gd\"\n" +
+            "}")
 
-    protected PEPProcInstVO start(String key, Map<String, Object> form) {
-        PEPProcInstVO pepProcInstVO = JSONUtil.parse(post('/workflow/process/' + key + "?access_token=" + token, JSONUtil.toJSON(form), HttpStatus.CREATED).getInputStream().getText(), PEPProcInstVO.class)
-        return pepProcInstVO
     }
 }
